67896acd0f6e09f291a1a7350a87aec93d350277
sagi <sagi21805@gmail.com>
2025-03-02 15:23:20 +0200

    checked memory load and passed on to second stage

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,62 @@
 #![no_std]  // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![allow(dead_code)]
+#![feature(optimize_attribute)]
+#![feature(ptr_as_ref_unchecked)]
 
-use core::panic::PanicInfo;
-use core::arch::{global_asm, asm};
+mod bios_enums;
+mod screen;
+mod disk;
+mod constants;
+
+use bios_enums::PacketSize;
+use core::{arch::global_asm, panic::PanicInfo};
+use disk::{DiskAddressPacket, MasterBootRecord};
+use constants::{MESSAGE, SECOND_STAGE_OFFSET, MASTER_BOOT_RECORD_OFFSET, BOOTABLE, NOT_BOOTABLE};
+use screen::MinimalWriter;
 
 global_asm!(include_str!("../asm/boot.s"));
 
-
-#[no_mangle]
-pub extern "C" fn print_char(c: u8) {
-    let ax = u16::from(c) | 0x0e00;
-    unsafe {
-        asm!("push bx", "mov bx, 0", "int 0x10", "pop bx", in("ax") ax);
-    }
-}
-
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle] 
+#[link_section = ".boot"]
 pub extern "C" fn first_stage() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    // let color: u16 = 0x0F00;
-    // let hello = "Hello cpp world!";
-    // let vga = 0xb8000 as *mut u16;
-    // let hello_ptr = hello.as_ptr();
-    // unsafe {
-    //     for i in 0..16 {
-    //         vga.add(i + 80).write(color | *(hello_ptr.add(i)) as u16);        
-    //     }
-    // }
-    for char in "Hello World!".chars() {
-        print_char(char as u8);
-    }
+    
+    MinimalWriter::print(MESSAGE);
+    let mbr = unsafe { 
+        (MASTER_BOOT_RECORD_OFFSET as *mut MasterBootRecord).as_mut_unchecked() 
+    };
+    let packet = DiskAddressPacket::new(
+        PacketSize::Default,
+        128, 
+        SECOND_STAGE_OFFSET as u32, 
+        1
+    );
+    packet.load();
+    second_stage(mbr);
+    
+
     loop {}
 }
 
-#[cold]
-#[inline(never)]
 #[no_mangle]
-pub extern "C" fn fail(code: u8) -> ! {
-    // print_char(b'!');
-    // print_char(code);
-    loop {
-        // hlt()
+#[optimize(none)]
+#[link_section = ".second_stage"]
+pub extern "C" fn second_stage(mbr: &mut MasterBootRecord) {
+    MinimalWriter::print("Entering Second Stage");
+    for entry in mbr.entries.iter() {
+        if entry.bootable == 0x80 {
+            MinimalWriter::print(BOOTABLE);
+        } 
+        else {
+            MinimalWriter::print(NOT_BOOTABLE);
+        }
     }
 }
+
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    // print(_info.message().as_str().unwrap());
     loop {}
 }
