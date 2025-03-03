4af50ee83f571845d62fa787317db834a984abbc
sagi <sagi21805@gmail.com>
2025-03-03 09:33:48 +0200

    added expiremental screen that still don't worl

--- src/bios_enums.rs
+++ src/bios_enums.rs
@@ -7,21 +7,41 @@ pub enum Interrupts {
 pub enum Video {
     SetMode = 0x0,
     DisplayChar = 0xE,
-    DisplayStr = 0x13
+    DisplayStr = 0x13,
 }
 
 #[allow(non_camel_case_types)]
 pub enum VideoModes {
     /// VGA Common Text Mode ->
-    /// 
+    ///
     /// Text resulotion 80x25
-    /// 
+    ///
     /// PixelBox resulotion 9x16
-    /// 
+    ///
     /// Pixel Resulption 720x400
     VGA_TX_80X25_PB_9X16_PR_720X400 = 0x3,
 }
 
 pub enum PacketSize {
     Default = 0x10,
+}
+
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,36 +1,42 @@
-use core::arch::asm;
-use crate::{bios_enums::{Interrupts, Video, VideoModes}, constants::{SCREEN_HEIGHT, SCREEN_WIDTH}};
+use crate::{
+    bios_enums::{Color, Interrupts, Video, VideoModes}, 
+    constants::{SCREEN_HEIGHT, SCREEN_WIDTH, VGA_BUFFER_PTR}
+};
+use core::{arch::asm, iter::Scan};
 
+#[repr(transparent)]
 pub struct MinimalWriter;
 
 impl MinimalWriter {
 
-    #[no_mangle]
     #[link_section = ".screen_minimal"]
+    #[cfg(feature = "stage-1-2")]
     fn print_char(char: u8) {
         unsafe {
             asm!(
                 "mov ah, {0}",
-                "int {1}",         
+                "int {1}",
                 const Video::DisplayChar as u8,
-                const Interrupts::VIDEO as u8, 
+                const Interrupts::VIDEO as u8,
                 in("al") char,
                 options(nostack, nomem)
             );
         }
     }
-    
-    #[no_mangle]
+
+    #[inline(never)]
     #[link_section = ".screen_minimal"]
+    #[cfg(feature = "stage-1-2")]
     pub fn print(str: &str) {
         for char in str.bytes() {
             MinimalWriter::print_char(char);
         }
     }
 
-    #[no_mangle]
-    #[link_section = ".screen"]
-    pub fn set_video_mode() {
+    #[link_section = ".kernel_function"]
+    #[cfg(feature = "stage-3")]
+    pub fn set_vga_text_mode() {
+        MinimalWriter::print("Entered Vga mode");
         unsafe {
             asm!(
                 "mov ah, {0}",
@@ -42,16 +48,117 @@ impl MinimalWriter {
             )
         }
     }
+}
+
+#[repr(transparent)]
+pub struct ColorCode(u8);
+
+impl ColorCode {
+
+    #[link_section = ".kernel_function"]
+    #[cfg(feature = "stage-3")]
+    pub const fn new(foreground: Color, background: Color) -> Self {
+        Self((background as u8) << 4 | (foreground as u8))
+    }
+
+
+    #[link_section = ".kernel_function"]
+    #[cfg(feature = "stage-3")]
+    const fn default() -> Self {
+        ColorCode((Color::Black as u8) << 4 | (Color::Yellow as u8))
+    }
 
 }
 
-pub struct Writer {
-    screen_buffer: [[u8; SCREEN_WIDTH]; SCREEN_HEIGHT],
+impl Clone for ColorCode {
 
+    #[link_section = ".kernel_function"]
+    #[cfg(feature = "stage-3")]
+    fn clone(&self) -> ColorCode {
+        ColorCode(self.0)
+    }
+}
+
+impl Copy for ColorCode { }
+
+
+#[repr(C)]
+pub struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+impl ScreenChar {
+    #[link_section = ".kernel_function"]
+    #[cfg(feature = "stage-3")]
+    const fn default() -> Self {
+        Self {
+            ascii_character: b'A',
+            color_code: ColorCode::default(),
+        }
+    }
+
+    #[link_section = ".kernel_function"]
+    #[cfg(feature = "stage-3")]
+    pub const fn new(character: u8, color: ColorCode) -> Self {
+        Self {
+            ascii_character: character,
+            color_code: color,
+        }
+    }
+}
+
+impl Clone for ScreenChar {
+
+    #[link_section = ".kernel_function"]
+    #[cfg(feature = "stage-3")]
+    fn clone(&self) -> Self {
+        Self {
+            ascii_character: self.ascii_character,
+            color_code: self.color_code.clone()
+        }
+    }
+}
+
+impl Copy for ScreenChar {
+
+}
+
+
+pub struct Writer {
+    screen: *mut ScreenChar,
+    col: usize,
+    row: usize,
 }
 
 impl Writer {
 
-    
+    #[cfg(feature = "stage-3")]
+    #[link_section = ".kernel_function"]
+    #[allow(static_mut_refs)]
+    pub fn new() -> Self {
+        MinimalWriter::set_vga_text_mode();
+        Self {
+            screen: VGA_BUFFER_PTR, // Dangerous and requires careful handling (static mut ref)
+            col: 0,
+            row: 0,
+        }
+    }
 
+    // #[link_section = ".kernel_function"]
+    // pub fn print(&mut self, message: &str, color: ColorCode) {
+    //     for char in message.bytes() {
+    //         self.screen[self.col][self.row] = ScreenChar::new(char, color);
+    //         self.col += 1;
+    //         if self.col >= SCREEN_WIDTH {
+    //             self.col = 0;
+    //             self.row += 1;
+    //         }
+    //         if self.row >= SCREEN_HEIGHT {
+    //             self.col = 0;
+    //             self.row = 0;
+    //         }
+    //         // MinimalWriter::print("Entered Vga mode");
+    //     }   
+    // }
 }
