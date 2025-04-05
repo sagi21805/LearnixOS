905926a08241b4ec0548b120103f9f3d496c930a
sagi <sagi21805@gmail.com>
2025-04-05 21:21:48 +0300

    added features

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,4 +1,11 @@
 #![no_std]
+#![feature(macro_metavar_expr_concat)]
+#![feature(adt_const_params)]
+#![feature(ptr_alignment_type)]
+#![feature(unsafe_cell_access)]
+
+
+#[cfg(feature = "screen")]
+pub mod screen;
 
 pub mod structures;
-pub mod screen;
