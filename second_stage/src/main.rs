562589c616a6d2de8d1788c6408a5bb6695887b1
sagi <sagi21805@gmail.com>
2025-04-10 23:39:03 +0300

    stage2 is now entering long mode

--- second_stage/src/main.rs
+++ second_stage/src/main.rs
@@ -4,34 +4,50 @@
 #![feature(optimize_attribute)]
 #![feature(ptr_as_ref_unchecked)]
 #![allow(static_mut_refs)]
-
-use constants::enums::Color;
-use core::panic::PanicInfo;
-use utils::{
-    print, println,
-    screen::color_code::ColorCode
+#![feature(unsafe_cell_access)]
+#![feature(ptr_alignment_type)]
+use constants::{
+    enums::Sections,
+    addresses::KERNEL_OFFSET
 };
+use core::{
+    panic::PanicInfo,
+    arch::asm,
+};
+use utils::structures::{global_descriptor_table::{GlobalDescriptorTable, GlobalDescriptorTableRegister32}, paging};
 
+static GLOBAL_DESCRIPTOR_TABLE_LONG_MODE: GlobalDescriptorTable = GlobalDescriptorTable::long_mode(); 
+static GLOBAL_DESCRIPTOR_TABLE_REGISTER_LONG: GlobalDescriptorTableRegister32 = GlobalDescriptorTableRegister32 {
+    limit: 24 - 1,
+    base: &GLOBAL_DESCRIPTOR_TABLE_LONG_MODE as *const GlobalDescriptorTable,
+};
 
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".start")]
 #[allow(unsafe_op_in_unsafe_fn)]
 pub unsafe extern "C" fn _start() -> ! {
-    let dn = core::ptr::read((0x7c00 - 2) as *const u16);
-    println!("Display Something");
-    println!("Stack Pointer: 0x{:x}", dn ; color = ColorCode::new(Color::Yellow, Color::Black));
-    println!("size: {}", (size_of::<usize>()));
-    // WRITER.clear();
-    // print!("Hello World");
+    // panic!("test");
+    asm!(
+        "mov eax, 0x10",
+        "mov ds, eax",
+    );
+    paging::enable();
+    asm!(
+        "lgdt [{}]",
+        in(reg) &GLOBAL_DESCRIPTOR_TABLE_REGISTER_LONG,
+    );
+    asm!(
+        "ljmp ${section}, ${next_stage}",
+        section = const Sections::KernelCode as u8,
+        next_stage = const KERNEL_OFFSET,
+        options(att_syntax)
+    );
+
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
 unsafe fn panic(_info: &PanicInfo) -> ! {
-    print!("[");
-    print!("FAIL" ; color = ColorCode::default());
-    print!("]: ");
-    println!("{}", _info);
     loop {}
 }
