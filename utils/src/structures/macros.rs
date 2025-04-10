f708221d6c953aa10263ccec1f4d4c48dbd71eb1
sagi <sagi21805@gmail.com>
2025-04-10 23:44:05 +0300

    added a macro for math operations in a wrapper struct

--- utils/src/structures/macros.rs
+++ utils/src/structures/macros.rs
@@ -1,4 +1,3 @@
-
 #[macro_export]
 macro_rules! flag {
     ($bit_name:ident, $bit_number:literal) => {
@@ -25,38 +24,54 @@ macro_rules! impl_math_ops {
     ($struct_name:ident, $inner_type:ty) => {
         impl core::ops::Add<$inner_type> for $struct_name {
             type Output = Self;
-            fn add(self, rhs: $inner_type) -> Self::Output { Self(self.0 + rhs) }
+            fn add(self, rhs: $inner_type) -> Self::Output {
+                Self(self.0 + rhs)
+            }
         }
 
         impl core::ops::AddAssign<$inner_type> for $struct_name {
-            fn add_assign(&mut self, rhs: $inner_type) { self.0 += rhs; }
+            fn add_assign(&mut self, rhs: $inner_type) {
+                self.0 += rhs;
+            }
         }
 
         impl core::ops::Sub<$inner_type> for $struct_name {
             type Output = Self;
-            fn sub(self, rhs: $inner_type) -> Self::Output { Self(self.0 - rhs) }
+            fn sub(self, rhs: $inner_type) -> Self::Output {
+                Self(self.0 - rhs)
+            }
         }
 
         impl core::ops::SubAssign<$inner_type> for $struct_name {
-            fn sub_assign(&mut self, rhs: $inner_type) { self.0 -= rhs; }
+            fn sub_assign(&mut self, rhs: $inner_type) {
+                self.0 -= rhs;
+            }
         }
 
         impl core::ops::Mul<$inner_type> for $struct_name {
             type Output = Self;
-            fn mul(self, rhs: $inner_type) -> Self::Output { Self(self.0 * rhs) }
+            fn mul(self, rhs: $inner_type) -> Self::Output {
+                Self(self.0 * rhs)
+            }
         }
 
         impl core::ops::MulAssign<$inner_type> for $struct_name {
-            fn mul_assign(&mut self, rhs: $inner_type) { self.0 *= rhs; }
+            fn mul_assign(&mut self, rhs: $inner_type) {
+                self.0 *= rhs;
+            }
         }
 
         impl core::ops::Div<$inner_type> for $struct_name {
             type Output = Self;
-            fn div(self, rhs: $inner_type) -> Self::Output { Self(self.0 / rhs) }
+            fn div(self, rhs: $inner_type) -> Self::Output {
+                Self(self.0 / rhs)
+            }
         }
 
         impl core::ops::DivAssign<$inner_type> for $struct_name {
-            fn div_assign(&mut self, rhs: $inner_type) { self.0 /= rhs; }
+            fn div_assign(&mut self, rhs: $inner_type) {
+                self.0 /= rhs;
+            }
         }
     };
 }
