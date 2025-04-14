8cfa6d11703a46f5644350f68487e71cfb149110
sagi <sagi21805@gmail.com>
2025-04-14 23:02:16 +0300

    moved macros that are relevant only to this file to the files

--- cpu_utils/src/structures/paging/address_types.rs
+++ cpu_utils/src/structures/paging/address_types.rs
@@ -1,58 +1,158 @@
-use super::get_current_page_table;
-use crate::impl_math_ops;
-use constants::enums::PageSize;
+/// This is a simple implementation of a physical and virtual addresses.
+/// 
+/// This interface is mainly used to provide explicit meaning for when a physical address is needed and when a virtual one
+/// 
+/// This module implements some very basic commands for the addresses and more rich and useful interface will be added in the kernel 
+/// -------------------------------------------------------------------------------------------------------------
 
-#[derive(Clone)]
+/// This macro will generate simple mathematical operations for wrapping structs
+///
+/// ```rust
+/// struct Example(u32);
+///
+/// impl_math_ops!(Example, u32)
+/// ```
+///
+/// The following code example will generate the following methods for the Example struct
+///
+/// `add`,
+/// `add_assign`,
+/// `sub`,
+/// `sub_assign`,
+/// `mul`,
+/// `mul_assign`,
+/// `div`,
+/// `div_assign`
+macro_rules! impl_math_ops {
+    ($struct_name:ident, $inner_type:ty) => {
+        impl core::ops::Add<$inner_type> for $struct_name {
+            type Output = Self;
+            fn add(self, rhs: $inner_type) -> Self::Output {
+                Self(self.0 + rhs)
+            }
+        }
+
+        impl core::ops::AddAssign<$inner_type> for $struct_name {
+            fn add_assign(&mut self, rhs: $inner_type) {
+                self.0 += rhs;
+            }
+        }
+
+        impl core::ops::Sub<$inner_type> for $struct_name {
+            type Output = Self;
+            fn sub(self, rhs: $inner_type) -> Self::Output {
+                Self(self.0 - rhs)
+            }
+        }
+
+        impl core::ops::SubAssign<$inner_type> for $struct_name {
+            fn sub_assign(&mut self, rhs: $inner_type) {
+                self.0 -= rhs;
+            }
+        }
+
+        impl core::ops::Mul<$inner_type> for $struct_name {
+            type Output = Self;
+            fn mul(self, rhs: $inner_type) -> Self::Output {
+                Self(self.0 * rhs)
+            }
+        }
+
+        impl core::ops::MulAssign<$inner_type> for $struct_name {
+            fn mul_assign(&mut self, rhs: $inner_type) {
+                self.0 *= rhs;
+            }
+        }
+
+        impl core::ops::Div<$inner_type> for $struct_name {
+            type Output = Self;
+            fn div(self, rhs: $inner_type) -> Self::Output {
+                Self(self.0 / rhs)
+            }
+        }
+
+        impl core::ops::DivAssign<$inner_type> for $struct_name {
+            fn div_assign(&mut self, rhs: $inner_type) {
+                self.0 /= rhs;
+            }
+        }
+    };
+}
+
+macro_rules! impl_common_address_functions {
+    ($struct_name:ident) => {
+        impl $struct_name {
+            /// Create new instance from an address
+            ///
+            /// # Safety
+            /// There is no check on the last bits of the address (bit 63-48 must be copies of bit 47)
+            ///
+            /// `This method is auto-generated`
+            pub const unsafe fn new_unchecked(address: usize) -> Self {
+                Self(address)
+            }
+
+            /// Create new instance from an address, copies bit 47 to bits 63-48
+            ///
+            /// `This method is auto-generated`
+            pub const fn new(address: usize) -> Self {
+                Self((address << 16) >> 16)
+            }
+
+            /// Create new instance with the zero address
+            pub const fn zero() -> Self {
+                Self(0)
+            }
+
+            #[inline]
+            /// Return the underlying usize
+            pub const fn as_usize(&self) -> usize {
+                self.0
+            }
+
+            #[inline]
+            /// Checks if this address is aligned to a certain alignment
+            pub const fn is_aligned(&self, alignment: core::ptr::Alignment) -> bool {
+                self.0 & (alignment.as_usize() - 1) == 0
+            }
+
+            #[inline]
+            /// Return the underlying number as mutable pointer to data
+            ///
+            /// # Safety
+            /// This method returns a mutable pointer without checking if this address is used or not
+            pub const unsafe fn as_ptr_mut(&self) -> *mut u8 {
+                self.0 as *mut u8
+            }
+
+            #[inline]
+            /// Return the underlying number as immutable pointer to data
+            pub const fn as_ptr(&self) -> *const u8 {
+                self.0 as *const u8
+            }
+        }
+    };
+}
+
+trait Address { }
+
+#[derive(Clone, Debug)]
 pub struct PhysicalAddress(usize);
 
 impl_math_ops!(PhysicalAddress, usize);
 
-#[derive(Clone)]
+impl_common_address_functions!(PhysicalAddress);
+
+#[derive(Clone, Debug)]
 pub struct VirtualAddress(usize);
 
 impl_math_ops!(VirtualAddress, usize);
 
-impl PhysicalAddress {
-    pub const fn new(address: usize) -> Self {
-        Self(address)
-    }
+impl_common_address_functions!(VirtualAddress);
 
-    pub const fn zero() -> Self {
-        Self(0)
-    }
-
-    #[inline]
-    pub const fn as_usize(&self) -> usize {
-        self.0
-    }
-
-    pub fn map(&self, address: VirtualAddress, page_size: PageSize) {
-        address.map(self.clone(), page_size)
-    }
-}
 
 impl VirtualAddress {
-    pub const unsafe fn new_unchecked(address: usize) -> Self {
-        Self(address)
-    }
-    pub const fn new(address: usize) -> Self {
-        // Bitshift is done to make sure bits 63-48 are equal to bit 47
-        Self((address << 16) >> 16)
-    }
 
-    // This function will obtain the page table automatically from cr3
-    pub fn map(&self, address: PhysicalAddress, page_size: PageSize) {
-        let mut table = unsafe { get_current_page_table() };
-
-        for table_number in ((page_size.clone() as usize + 1)..=4).rev() {
-            if table.entries[self.nth_pt_index(table_number)].present() {
-                table = table.entries[self.nth_pt_index(table_number)].get_next_table_mut()
-            } else {
-                table.entries[self.nth_pt_index(table_number)].create_new_table();
-            }
-        }
-        table.entries[self.nth_pt_index(page_size as usize)].set_frame_address(address);
-    }
 
     pub fn translate(&self) -> PhysicalAddress {
         todo!()
@@ -75,7 +175,7 @@ impl VirtualAddress {
     pub const fn pt1_index(&self) -> usize {
         (self.0 >> 12) & 0o777
     }
-    // index of the n_th paage table
+    // index of the n_th page table
     pub const fn nth_pt_index(&self, n: usize) -> usize {
         if n > 4 || n < 1 {
             panic!("There are only 4 page tables, you tried to index table out of range");
