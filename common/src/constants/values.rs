e57a3e230ece3aec7f03db7c205385ac45ce99d9
sagi <sagi21805@gmail.com>
2025-06-14 12:16:12 +0300

    added mib and kib sizes

--- common/src/constants/values.rs
+++ common/src/constants/values.rs
@@ -4,7 +4,10 @@ pub const PAGE_DIRECTORY_ENTRIES: usize = 512;
 pub const REGULAR_PAGE_SIZE: usize = 4096;
 pub const BIG_PAGE_SIZE: usize = REGULAR_PAGE_SIZE * PAGE_DIRECTORY_ENTRIES;
 pub const HUGE_PAGE_SIZE: usize = BIG_PAGE_SIZE * PAGE_DIRECTORY_ENTRIES;
-
+#[allow(non_upper_case_globals)]
+pub const KiB: usize = 1024;
+#[allow(non_upper_case_globals)]
+pub const MiB: usize = 1024 * 1024;
 pub const REGULAR_PAGE_ALIGNMENT: Alignment =
     unsafe { Alignment::new_unchecked(REGULAR_PAGE_SIZE) };
 pub const BIG_PAGE_ALIGNMENT: Alignment = unsafe { Alignment::new_unchecked(BIG_PAGE_SIZE) };
