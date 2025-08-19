a87aa5e79ac932a1793fb77d02398a9314f927d1
sagi21805 <sagi21805@gmail.com>
2025-08-19 23:49:59 +0300

    changed into a default handler that prints stack frame

--- kernel/src/drivers/timer.rs
+++ kernel/src/drivers/timer.rs
@@ -1,6 +1,9 @@
-use crate::print;
 use cpu_utils::structures::interrupt_descriptor_table::InterruptStackFrame;
 
-pub extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    print!("{:?}", _stack_frame);
+use crate::println;
+
+#[unsafe(no_mangle)] // keep the symbol name
+#[inline(never)] // prevent inlining/removal due to optimization
+pub extern "x86-interrupt" fn default_handler(_stack_frame: InterruptStackFrame) {
+    println!("{:x?}", _stack_frame);
 }
