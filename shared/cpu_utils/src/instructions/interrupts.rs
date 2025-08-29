6529da29fb7ca68a1d3154ff4d356fea029b46a3
sagi21805 <sagi21805@gmail.com>
2025-08-29 10:57:50 +0300

    Added instruction for enabling and disabling interrupts

--- shared/cpu_utils/src/instructions/interrupts.rs
+++ shared/cpu_utils/src/instructions/interrupts.rs
@@ -1,1 +1,22 @@
+use core::arch::asm;
 
+/// x86/x86_64-only.
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[inline(always)]
+pub unsafe fn enable() {
+    unsafe { asm!("sti", options(nostack, preserves_flags)) };
+}
+
+/// x86/x86_64-only.
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[inline(always)]
+pub unsafe fn disable() {
+    unsafe { asm!("cli", options(nostack, preserves_flags)) };
+}
+
+/// x86/x86_64-only. Halts the CPU until the next external interrupt.
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[inline(always)]
+pub unsafe fn hlt() {
+    unsafe { asm!("hlt", options(nostack, nomem)) };
+}
