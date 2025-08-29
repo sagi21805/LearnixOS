2f583d47fecbbb8aba53e75eb960d4da688544fb
sagi21805 <sagi21805@gmail.com>
2025-08-29 19:46:56 +0300

    renamed ports

--- shared/common/src/enums/ports.rs
+++ shared/common/src/enums/ports.rs
@@ -3,9 +3,9 @@
 pub enum Port {
     KeyboardData = 0x60,
     KeyboardStatus = 0x64,
-    PrimaryPicCmd = 0x20,
-    PrimaryPicData = 0x21,
-    SecondaryPicCmd = 0xA0,
-    SecondaryPicData = 0xA1,
+    MasterPicCmd = 0x20,
+    MasterPicData = 0x21,
+    SlavePicCmd = 0xA0,
+    SlavePicData = 0xA1,
     IOWait = 0x80,
 }
