59a53ac405fd723e0a3de4e5f1de30828a308d53
sagi21805 <sagi21805@gmail.com>
2025-08-29 14:13:03 +0300

    implemented segment registers, rflags and tlb functionality

--- shared/cpu_utils/src/registers/control.rs
+++ shared/cpu_utils/src/registers/control.rs
@@ -1,4 +1,7 @@
-use crate::registers::macros::{impl_reg_read_write_u32, impl_reg_read_write_u64};
+#[cfg(target_arch = "x86")]
+use crate::registers::macros::impl_reg_read_write_u32;
+#[cfg(target_arch = "x86_64")]
+use crate::registers::macros::impl_reg_read_write_u64;
 
 #[cfg(target_arch = "x86_64")]
 impl_reg_read_write_u64!(cr3);
---
+++ shared/cpu_utils/src/registers/segment.rs
@@ -0,0 +1,5 @@
+use crate::registers::macros::impl_reg_read_write_u16;
+
+impl_reg_read_write_u16!(ss);
+impl_reg_read_write_u16!(ds);
+impl_reg_read_write_u16!(es);
