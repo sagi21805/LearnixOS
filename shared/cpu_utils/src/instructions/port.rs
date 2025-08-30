7e823ecf2d829679239608fd7949b6bfd5fdaf30
sagi21805 <sagi21805@gmail.com>
2025-08-30 16:09:28 +0300

    changed the interface for writing to ports

--- shared/cpu_utils/src/instructions/port.rs
+++ shared/cpu_utils/src/instructions/port.rs
@@ -1,86 +1,92 @@
 use common::enums::Port;
 use core::arch::asm;
+use extend::ext;
 
-#[inline(always)]
-pub unsafe fn outb(port: Port, val: u8) {
-    unsafe {
-        asm!(
-            "out dx, al",
-            in("dx") port as u16,
-            in("al") val,
-            options(nostack, preserves_flags),
-        );
+#[ext]
+impl Port {
+    #[inline(always)]
+    unsafe fn outb(self, val: u8) {
+        unsafe {
+            asm!(
+                "out dx, al",
+                in("dx") self as u16,
+                in("al") val,
+                options(nostack, preserves_flags),
+            );
+        }
     }
-}
 
-#[inline(always)]
-pub unsafe fn outw(port: Port, val: u16) {
-    unsafe {
-        asm!(
-            "out dx, ax",
-            in("dx") port as u16,
-            in("ax") val,
-            options(nostack, preserves_flags),
-        );
+    #[inline(always)]
+    unsafe fn outw(self, val: u16) {
+        unsafe {
+            asm!(
+                "out dx, ax",
+                in("dx") self as u16,
+                in("ax") val,
+                options(nostack, preserves_flags),
+            );
+        }
     }
-}
 
-#[inline(always)]
-pub unsafe fn outl(port: Port, val: u32) {
-    unsafe {
-        asm!(
-            "out dx, eax",
-            in("dx") port as u16,
-            in("eax") val,
-            options(nostack, preserves_flags),
-        );
+    #[inline(always)]
+    unsafe fn outl(self, val: u32) {
+        unsafe {
+            asm!(
+                "out dx, eax",
+                in("dx") self as u16,
+                in("eax") val,
+                options(nostack, preserves_flags),
+            );
+        }
     }
-}
 
-/// IN instructions
-#[inline(always)]
-pub unsafe fn inb(port: Port) -> u8 {
-    let mut val: u8;
-    unsafe {
-        asm!(
-            "in al, dx",
-            in("dx") port as u16,
-            out("al") val,
-            options(nostack, preserves_flags),
-        );
+    /// IN instructions
+    #[inline(always)]
+    unsafe fn inb(self) -> u8 {
+        let mut val: u8;
+        unsafe {
+            asm!(
+                "in al, dx",
+                in("dx") self as u16,
+                out("al") val,
+                options(nostack, preserves_flags),
+            );
+        }
+        val
     }
-    val
-}
 
-#[inline(always)]
-pub unsafe fn inw(port: Port) -> u16 {
-    let mut val: u16;
-    unsafe {
-        asm!(
-            "in ax, dx",
-            in("dx") port as u16,
-            out("ax") val,
-            options(nostack, preserves_flags),
-        );
+    #[inline(always)]
+    unsafe fn inw(self) -> u16 {
+        let mut val: u16;
+        unsafe {
+            asm!(
+                "in ax, dx",
+                in("dx") self as u16,
+                out("ax") val,
+                options(nostack, preserves_flags),
+            );
+        }
+        val
     }
-    val
-}
 
-#[inline(always)]
-pub unsafe fn inl(port: Port) -> u32 {
-    let mut val: u32;
-    unsafe {
-        asm!(
-            "in eax, dx",
-            in("dx") port as u16,
-            out("eax") val,
-            options(nostack, preserves_flags),
-        );
+    #[inline(always)]
+    unsafe fn inl(self) -> u32 {
+        let mut val: u32;
+        unsafe {
+            asm!(
+                "in eax, dx",
+                in("dx") self as u16,
+                out("eax") val,
+                options(nostack, preserves_flags),
+            );
+        }
+        val
     }
-    val
-}
 
-#[inline(always)]
-pub unsafe fn iowait() {
-    unsafe { outb(Port::IOWait, 0) };
+    #[inline(always)]
+    unsafe fn iowait() {
+        unsafe {
+            Port::IOWait.outb(0);
+        };
+    }
 }
