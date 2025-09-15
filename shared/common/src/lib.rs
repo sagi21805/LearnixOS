1694c0844fb56d3eadcc1e7b8379f47c3c3a9b47
sagi21805 <sagi21805@gmail.com>
2025-09-15 19:43:19 +0300

    added fake global allocator so I could extern alloc and use vec with my allocator

--- shared/common/src/lib.rs
+++ shared/common/src/lib.rs
@@ -10,3 +10,26 @@ pub mod constants;
 pub mod enums;
 pub mod error;
 pub mod ring_buffer;
+struct FakeAllocator;
+
+unsafe impl core::alloc::GlobalAlloc for FakeAllocator {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        unsafe {
+            unsafe extern "Rust" {
+                unsafe fn fake_alloc_this_doesnt_exist(layout: core::alloc::Layout) -> *mut u8;
+            }
+            fake_alloc_this_doesnt_exist(layout)
+        }
+    }
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        unsafe {
+            unsafe extern "Rust" {
+                unsafe fn fake_dealloc_this_doesnt_exist(ptr: *mut u8, layout: core::alloc::Layout);
+            }
+            fake_dealloc_this_doesnt_exist(ptr, layout)
+        }
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: FakeAllocator = FakeAllocator;
