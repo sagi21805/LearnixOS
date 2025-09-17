385a507d0efc1edcfff8139b846864b3d5d84d11
sagi21805 <sagi21805@gmail.com>
2025-09-17 21:45:27 +0300

    added implementation for the cr2 register

--- shared/cpu_utils/src/registers/control.rs
+++ shared/cpu_utils/src/registers/control.rs
@@ -5,6 +5,8 @@ use crate::registers::macros::impl_reg_read_write_u64;
 
 #[cfg(target_arch = "x86_64")]
 impl_reg_read_write_u64!(cr3);
+#[cfg(target_arch = "x86_64")]
+impl_reg_read_write_u64!(cr2);
 
 #[cfg(target_arch = "x86")]
 impl_reg_read_write_u32!(cr3);
