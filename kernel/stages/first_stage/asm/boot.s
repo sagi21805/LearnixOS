54ff4f01b381da2dc820a1ed2a8c4adff576086b
sagi <sagi21805@gmail.com>
2025-10-21 23:02:06 +0300

    added ; to the comment to see if it stays the same on the webinclude

--- kernel/stages/first_stage/asm/boot.s
+++ kernel/stages/first_stage/asm/boot.s
@@ -1,13 +1,13 @@
 // ANCHOR: segment-zeroing
-// This will define a boot section for this asm code,
-// which we can put at the start of our binary.
+//; This will define a boot section for this asm code,
+//; which we can put at the start of our binary.
 .section .boot, "awx"
 .global start
 .code16
 start:
-    // Disable interrupts
+    //; Disable interrupts
     cli
-    // zero segment registers
+    //; zero segment registers
     xor ax, ax
     mov ds, ax
     mov es, ax
@@ -16,25 +16,25 @@ start:
     mov gs, ax
 // ANCHOR_END: segment-zeroing
 
-    // clear the direction flag (e.g. go forward in memory when using
-    // instructions like lodsb)
+    //; clear the direction flag (e.g. go forward in memory when using
+    //; instructions like lodsb)
     cld
 
-    // initialize stack
+    //; initialize stack
     mov sp, 0x7c00
 
-// Enable the A20 line via I/O Port 0x92
-// This method might not work on all motherboards
-// Use with care!
+//; Enable the A20 line via I/O Port 0x92
+//; This method might not work on all motherboards
+//; Use with care!
 enable_a20:
-    // Check if a20 is already enabled
+    //; Check if a20 is already enabled
     in al, 0x92
     test al, 2
 
-    // If so, skip the enabling code
+    //; If so, skip the enabling code
     jnz enable_a20_after
 
-    // Else, enable the a20 line
+    //; Else, enable the a20 line
     or al, 2
     and al, 0xFE
     out 0x92, al
@@ -42,15 +42,15 @@ enable_a20_after:
 
 check_int13h_extensions:
 
-    // Set function constants `dl` already contains the driver
+    //; Set function constants `dl` already contains the driver
     mov ah, 0x41
     mov bx, 0x55aa
     int 0x13
     jnc .int13_pass
-    // hlt system if there is no support
+    //; hlt system if there is no support
     hlt
 .int13_pass:
 
-// push disk number into the stack will be at 0x7bfe and call the first_stage function
+//; push disk number into the stack will be at 0x7bfe and call the first_stage function
 push dx
 call first_stage
