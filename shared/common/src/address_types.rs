8b92e702d4acd9c30e071595d9912fdaa3aec7ac
sagi21805 <sagi21805@gmail.com>
2025-11-15 19:23:41 +0200

    added anchors

--- shared/common/src/address_types.rs
+++ shared/common/src/address_types.rs
@@ -1,13 +1,17 @@
+use core::convert::identity;
+
 #[cfg(target_arch = "x86_64")]
 use crate::constants::PHYSICAL_MEMORY_OFFSET;
 
-use learnix_macros::CommonAddressFunctions;
-
+// ANCHOR: trait_imports
 use derive_more::{
     Add, AddAssign, AsMut, AsRef, Div, DivAssign, From, Mul, MulAssign,
     Sub, SubAssign,
 };
+use learnix_macros::CommonAddressFunctions;
+// ANCHOR_END: trait_imports
 
+// ANCHOR: physical_address
 #[derive(
     Clone,
     Debug,
@@ -28,7 +32,9 @@ use derive_more::{
 )]
 #[repr(C)]
 pub struct PhysicalAddress(usize);
+// ANCHOR_END: physical_address
 
+// ANCHOR: virtual_address
 #[derive(
     Clone,
     Debug,
@@ -49,8 +55,10 @@ pub struct PhysicalAddress(usize);
 )]
 #[repr(C)]
 pub struct VirtualAddress(usize);
+// ANCHOR_END: virtual_address
 
 impl VirtualAddress {
+    // ANCHOR: virtual_from_indexes
     #[allow(arithmetic_overflow)]
     pub const fn from_indexes(
         i4: usize,
@@ -60,10 +68,13 @@ impl VirtualAddress {
     ) -> Self {
         Self((i4 << 39) | (i3 << 30) | (i2 << 21) | (i1 << 12) | 0)
     }
+    // ANCHOR_END: virtual_from_indexes
 
+    // ANCHOR: virtual_from_indices
     pub const fn from_indices(indices: [usize; 4]) -> Self {
         Self::from_indexes(indices[0], indices[1], indices[2], indices[3])
     }
+    // ANCHOR_END: virtual_from_indices
 
     /// indexing for the n_th page table
     ///
@@ -74,9 +85,11 @@ impl VirtualAddress {
     /// 2 -> index of 2nd table
     ///
     /// 1 -> index of 1st table
+    // ANCHOR: virtual_nth_pt_index_unchecked
     pub const unsafe fn nth_pt_index_unchecked(&self, n: usize) -> usize {
         (self.0 >> (39 - 9 * (4 - n))) & 0o777
     }
+    // ANCHOR_END: virtual_nth_pt_index_unchecked
 
     /// Reverse indexing for the address:
     ///
@@ -87,10 +100,12 @@ impl VirtualAddress {
     /// 2 -> index of 2nd table
     ///
     /// 3 -> index of 1st table
+    // ANCHOR: virtual_rev_nth_index_unchecked
     #[allow(arithmetic_overflow)]
     pub const fn rev_nth_index_unchecked(&self, n: usize) -> usize {
         (self.0 >> (39 - (9 * n))) & 0o777
     }
+    // ANCHOR_END: virtual_rev_nth_index_unchecked
 
     // pub fn translate(&self) -> Option<PhysicalAddress> {
     //     let mut current_table =
@@ -112,9 +127,11 @@ impl VirtualAddress {
 }
 
 impl PhysicalAddress {
+    // ANCHOR: physical_translate
     #[inline]
     #[cfg(target_arch = "x86_64")]
     pub const fn translate(&self) -> VirtualAddress {
         VirtualAddress(self.0 + PHYSICAL_MEMORY_OFFSET)
     }
+    // ANCHOR_END: physical_translate
 }
