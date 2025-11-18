6a86c67a6e9ba5613f98f9aacd9476c34805ff5f
sagi <sagi21805@gmail.com>
2025-11-18 22:06:15 +0200

    renamed second stage start function

--- kernel/stages/second_stage/src/main.rs
+++ kernel/stages/second_stage/src/main.rs
@@ -10,16 +10,18 @@ use common::{constants::addresses::KERNEL_OFFSET, enums::Sections};
 use core::{arch::asm, panic::PanicInfo};
 use cpu_utils::structures::global_descriptor_table::GlobalDescriptorTableLong;
 
+// ANCHOR: gdt_long
 static GLOBAL_DESCRIPTOR_TABLE_LONG_MODE: GlobalDescriptorTableLong =
     GlobalDescriptorTableLong::default();
+// ANCHOR_END: gdt_long
 
+// ANCHOR: _start
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".start")]
 #[allow(unsafe_op_in_unsafe_fn)]
-pub unsafe extern "C" fn _start() -> ! {
+pub unsafe extern "C" fn second_stage() -> ! {
     // Set data segment register
     asm!("mov eax, 0x10", "mov ds, eax",);
-
     // Enable paging and load page tables with an identity
     // mapping
     #[cfg(target_arch = "x86")]
@@ -36,8 +38,8 @@ pub unsafe extern "C" fn _start() -> ! {
     );
     loop {}
 }
+// ANCHOR_END: _start
 
-/// This function is called on panic.
 #[panic_handler]
 unsafe fn panic(_info: &PanicInfo) -> ! {
     loop {}
