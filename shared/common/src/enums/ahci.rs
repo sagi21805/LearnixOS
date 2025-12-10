96c8334c9f7efac21f3aaa1c1f7d73d0c1f58bdd
sagi21805 <sagi21805@gmail.com>
2025-12-10 22:39:52 +0200

    added anchors

--- shared/common/src/enums/ahci.rs
+++ shared/common/src/enums/ahci.rs
@@ -1,6 +1,7 @@
 use derive_more::Display;
 use num_enum::{TryFromPrimitive, UnsafeFromPrimitive};
 
+// ANCHOR: AHCIInterfaceSpeed
 #[derive(Display, Clone, Copy, TryFromPrimitive, UnsafeFromPrimitive)]
 #[repr(u8)]
 pub enum AHCIInterfaceSpeed {
@@ -11,7 +12,9 @@ pub enum AHCIInterfaceSpeed {
     #[display("Gen1: 6.0Gb/s")]
     Gen3 = 3,
 }
+// ANCHOR_END: AHCIInterfaceSpeed
 
+// ANCHOR: InterfaceCommunicationControl
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, TryFromPrimitive)]
 pub enum InterfaceCommunicationControl {
@@ -23,3 +26,4 @@ pub enum InterfaceCommunicationControl {
     #[num_enum(alternatives = [3..=5, 7, 9..=14])]
     Reserved = 0xf,
 }
+// ANCHOR_END: InterfaceCommunicationControl
