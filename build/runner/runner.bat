8562afbad42f43c6e2266e790b640db057aa8b75
sagi <sagi21805@gmail.com>
2025-12-15 19:10:05 +0200

    removed second disk with the same file, because of a locking issue tha occured on mac

--- build/runner/runner.bat
+++ build/runner/runner.bat
@@ -4,7 +4,5 @@
 qemu-system-x86_64 ^
     -M q35 ^
     -drive id=disk0,file=build/image.bin,if=none,format=raw ^
-    -drive id=disk1,file=build/image.bin,if=none,format=raw ^
     -device ide-hd,drive=disk0,bus=ide.0 ^
-    -device ide-hd,drive=disk1,bus=ide.1 ^
     -monitor stdio
--- build/runner/runner.sh
+++ build/runner/runner.sh
@@ -3,8 +3,5 @@
 qemu-system-x86_64 \
     -M q35 \
     -drive id=disk0,format=raw,file=build/image.bin,if=none \
-    -drive id=disk1,format=raw,file=build/image.bin,if=none \
-    -device ahci,id=ahci0 \
-    -device ide-hd,drive=disk0,bus=ahci0.0 \
-    -device ide-hd,drive=disk1,bus=ahci0.1 \
+    -device ide-hd,drive=disk0,bus=ide.0 \
     -monitor stdio
