5c73aa532b82fc7065723dbfb51c0e2d576fa537
sagi <sagi21805@gmail.com>
2026-01-12 00:11:29 +0200

    added an alloc function and used nonmax crate

--- kernel/src/memory/allocators/slab.rs
+++ kernel/src/memory/allocators/slab.rs
@@ -1,15 +1,19 @@
-use core::{fmt::Debug, mem::ManuallyDrop, num::NonZero};
+use core::{fmt::Debug, mem::ManuallyDrop};
 
-use common::constants::REGULAR_PAGE_SIZE;
+use common::{constants::REGULAR_PAGE_SIZE, write_volatile};
 
-use crate::alloc_pages;
+use nonmax::NonMaxU16;
+
+use crate::{alloc_pages, memory::page_descriptor::Unassigned};
 
 pub union SlabCaches {
     pub generic4: ManuallyDrop<SlabCache<u32>>,
+    pub slab_descriptor: ManuallyDrop<SlabDescriptor<Unassigned>>,
+    pub slab_cache: ManuallyDrop<SlabCache<Unassigned>>,
 }
 
 pub static SLABS: [SlabCaches; 1] = [SlabCaches {
-    generic4: ManuallyDrop::new(SlabCache::new()),
+    slab_descriptor: ManuallyDrop::new(SlabCache::new()),
 }];
 
 /// Preallocated object in the slab allocator.
@@ -19,7 +23,7 @@ pub static SLABS: [SlabCaches; 1] = [SlabCaches {
 /// overwrite by the objects data thus wasting no space on the free list.
 pub union PreallocatedObject<T: 'static + Sized> {
     pub allocated: ManuallyDrop<T>,
-    pub next_free_idx: Option<NonZero<u16>>,
+    pub next_free_idx: Option<NonMaxU16>,
 }
 
 impl<T> Debug for PreallocatedObject<T> {
@@ -31,7 +35,7 @@ impl<T> Debug for PreallocatedObject<T> {
 #[derive(Debug)]
 pub struct SlabDescriptor<T: 'static + Sized> {
     /// The index in the objects array of the next free objet
-    pub next_free_idx: usize,
+    pub next_free_idx: Option<NonMaxU16>,
     pub objects: &'static mut [PreallocatedObject<T>],
     pub next: Option<&'static mut SlabDescriptor<T>>,
 }
@@ -51,18 +55,37 @@ impl<T> SlabDescriptor<T> {
 
         for (i, object) in objects.iter_mut().enumerate() {
             *object = PreallocatedObject {
-                next_free_idx: Some(NonZero < i as u16 + 1),
+                next_free_idx: Some(unsafe {
+                    NonMaxU16::new_unchecked(i as u16 + 1)
+                }),
             }
         }
 
-        unsafe { objects.last().unwrap().next_free_idx }
+        objects.last_mut().unwrap().next_free_idx = None;
 
         SlabDescriptor {
-            next_free_idx: 0,
+            next_free_idx: Some(unsafe { NonMaxU16::new_unchecked(0) }),
             objects,
             next,
         }
     }
+
+    pub fn alloc_obj(&mut self, obj: T) -> &mut T {
+        debug_assert!(
+            self.next_free_idx.is_some(),
+            "Should always be some, because if not, slab is full"
+        );
+
+        let preallocated =
+            &mut self.objects[self.next_free_idx.unwrap().get() as usize];
+        self.next_free_idx = unsafe { preallocated.next_free_idx };
+
+        write_volatile!(preallocated.allocated, ManuallyDrop::new(obj));
+
+        unsafe { &mut preallocated.allocated }
+    }
+
+    pub unsafe fn dealloc_obj(&self, obj: &T) {}
 }
 
 #[derive(Debug)]
