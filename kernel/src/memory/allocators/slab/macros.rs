f628485be344d5cb724d6aa32004ffd2c778ebb1
sagi <sagi21805@gmail.com>
2026-01-14 23:30:53 +0200

    added implementation of the init function for all the slabs the are created, this is really useful because now each slab can implement it's own new function if necessary and it will call the dedicated function instead of a general one.

--- kernel/src/memory/allocators/slab/macros.rs
+++ kernel/src/memory/allocators/slab/macros.rs
@@ -5,7 +5,7 @@ macro_rules! register_slabs {
     };
 
     (@step $idx:expr; $head:ty, $($tail:ty),+) => {
-        impl $crate::slab::traits::SlabPosition for $head {
+        impl $crate::memory::allocators::slab::traits::SlabPosition for $head {
             const POSITION: usize = $idx;
         }
         $crate::register_slabs!(@step $idx + 1; $($tail),*);
@@ -23,20 +23,37 @@ macro_rules! register_slabs {
 #[macro_export]
 macro_rules! define_slab_system {
     ($($t:ty),* $(,)?) => {
+        use common::constants::REGULAR_PAGE_SIZE;
 
         $crate::register_slabs!($($t),*);
 
         const COUNT: usize = [$(stringify!($t)),*].len();
 
-        pub static mut SLABS: [
+        pub struct SlabAllocator {
+            slabs: [common::late_init::LateInit<SlabCache<$crate::memory::page_descriptor::Unassigned>>; COUNT]
+        }
 
-            common::late_init::LateInit<SlabCache<$crate::memory::page_descriptor::Unassigned>>; COUNT] = [
-            $(
-                {
-                    stringify!($t);
-                    common::late_init::LateInit::uninit()
+        impl SlabAllocator {
+            pub const fn new() -> Self {
+                Self {
+                    slabs: [
+                        $({
+                            let _ = stringify!($t);
+                            common::late_init::LateInit::uninit()
+                        }),*
+                    ]
                 }
-            ),*
-        ];
+            }
+
+            pub fn init(&'static mut self) {
+                $(
+                    let index = <$t>::POSITION;
+
+                    self.slabs[index].write(SlabCache::<$t>::new(
+                        size_of::<$t>().next_multiple_of(REGULAR_PAGE_SIZE) / REGULAR_PAGE_SIZE
+                    ).as_unassigned().clone());
+                )*
+            }
+        }
     }
 }
