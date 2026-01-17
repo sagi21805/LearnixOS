7fd37f61f8a830e1b15757031cba8023a34c8950
sagi <sagi21805@gmail.com>
2026-01-17 19:34:13 +0200

    fixed a bug where prev was not treated on next and prev

--- kernel/src/memory/allocators/buddy/meta.rs
+++ kernel/src/memory/allocators/buddy/meta.rs
@@ -1,4 +1,4 @@
-use core::ptr::NonNull;
+use core::{iter::ByRefSized, ptr::NonNull};
 
 use common::enums::BuddyOrder;
 
@@ -9,23 +9,45 @@ use crate::memory::{
     },
 };
 
-#[derive(Default, Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug)]
 pub struct BuddyBlockMeta {
-    // TODO CHANGE INTO REF BECAUSE IT CONSUMES LESS MEMORY
-    pub next: Option<NonNull<UnassignedPage>>,
-    pub prev: Option<NonNull<UnassignedPage>>,
-    pub order: Option<BuddyOrder>,
+    next: Option<NonNull<UnassignedPage>>,
+    prev: Option<NonNull<UnassignedPage>>,
+    order: Option<BuddyOrder>,
+}
+
+impl const Default for BuddyBlockMeta {
+    fn default() -> Self {
+        Self {
+            next: None,
+            prev: None,
+            order: None,
+        }
+    }
 }
 
 impl BuddyBlockMeta {
     pub fn detach<T: SlabPosition>(&mut self) -> Option<NonNull<Page<T>>> {
         let detached = self.next?; // None if there is no page to detach
+
         self.next = unsafe { detached.as_ref().buddy_meta.next };
+
+        if let Some(mut next) = self.next {
+            unsafe { next.as_mut().buddy_meta.prev = None }
+        }
+
         Some(detached.assign::<T>())
     }
 
     pub fn attach<T: SlabPosition>(&mut self, mut p: NonNull<Page<T>>) {
         unsafe { p.as_mut().buddy_meta.next = self.next };
+
+        if let Some(mut next) = self.next {
+            unsafe {
+                next.as_mut().buddy_meta.prev = Some(p.as_unassigned())
+            };
+        }
+
         self.next = Some(p.as_unassigned())
     }
 }
