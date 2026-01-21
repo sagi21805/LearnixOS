4adc63a2d7438333bce087f9470e3f698a0bd154
sagi <sagi21805@gmail.com>
2026-01-22 00:18:13 +0200

    added macro implementation of the generic structs and traits via macro

--- kernel/src/memory/allocators/slab.rs
+++ kernel/src/memory/allocators/slab.rs
@@ -3,45 +3,38 @@ pub mod descriptor;
 pub mod macros;
 pub mod traits;
 
+use common::address_types::VirtualAddress;
+use learnix_macros::generate_generics;
+
 use crate::{
     define_slab_system,
     memory::{
-        allocators::slab::{
-            cache::SlabCache,
-            descriptor::SlabDescriptor,
-            traits::{SlabCacheConstructor, SlabPosition},
+        allocators::{
+            extensions::VirtualAddressExt,
+            slab::{
+                cache::SlabCache,
+                descriptor::SlabDescriptor,
+                traits::{SlabCacheConstructor, SlabPosition},
+            },
         },
-        page_descriptor::Unassigned,
+        page_descriptor::{PAGES, Unassigned, UnassignedPage},
     },
 };
-use core::ptr::NonNull;
+use core::{
+    alloc::{AllocError, Allocator},
+    ptr::NonNull,
+};
 
-#[derive(Debug)]
-pub struct Generic8(pub usize);
-#[derive(Debug)]
-pub struct Generic16(pub [usize; 2]);
-#[derive(Debug)]
-pub struct Generic32(pub [usize; 4]);
-#[derive(Debug)]
-pub struct Generic64(pub [usize; 8]);
-#[derive(Debug)]
-pub struct Generic96(pub [usize; 12]);
-#[derive(Debug)]
-pub struct Generic128(pub [usize; 16]);
-#[derive(Debug)]
-pub struct Generic192(pub [usize; 24]);
-#[derive(Debug)]
-pub struct Generic256(pub [usize; 32]);
-#[derive(Debug)]
-pub struct Generic512(pub [usize; 64]);
-#[derive(Debug)]
-pub struct Generic1024(pub [usize; 128]);
-#[derive(Debug)]
-pub struct Generic2048(pub [usize; 256]);
-#[derive(Debug)]
-pub struct Generic4096(pub [usize; 512]);
-#[derive(Debug)]
-pub struct Generic8192(pub [usize; 1024]);
+pub trait Generic {
+    const START: usize;
+    const END: usize;
+
+    fn size(&self) -> usize;
+}
+
+generate_generics!(
+    8, 16, 32, 64, 96, 128, 192, 256, 512, 1024, 2048, 4096, 8192
+);
 
 define_slab_system!(
     SlabDescriptor<Unassigned>,
@@ -71,6 +64,21 @@ impl SlabAllocator {
         let mut slab = self.slab_of::<T>();
         unsafe { slab.as_mut().alloc() }
     }
+
+    pub fn kfree<T: SlabPosition>(&self, ptr: NonNull<T>) {
+        let index = UnassignedPage::index_of_page(unsafe {
+            VirtualAddress::new_unchecked(ptr.as_ptr() as usize)
+                .translate()
+        });
+
+        let page = unsafe { PAGES[index].assign::<T>().as_ref() };
+
+        if let Some(mut descriptor) = page.owner {
+            unsafe { descriptor.as_mut().dealloc(ptr) };
+        } else {
+            panic!("Object is freed from a page that has not owner!")
+        }
+    }
 }
 
 #[extend::ext]
@@ -80,6 +88,123 @@ impl NonNull<SlabDescriptor<Unassigned>> {
     }
 }
 
+#[extend::ext]
+pub impl<T: Generic> NonNull<T> {
+    fn into_u8(&self) -> NonNull<[u8]> {
+        unsafe {
+            let data = NonNull::new_unchecked(self.as_ptr() as *mut u8);
+            let size = self.as_ref().size();
+            NonNull::slice_from_raw_parts(data, size)
+        }
+    }
+
+    fn from_u8(data: NonNull<u8>) -> NonNull<T> {
+        unsafe { NonNull::new_unchecked(data.as_ptr() as *mut T) }
+    }
+}
+
+unsafe impl Allocator for SlabAllocator {
+    fn allocate(
+        &self,
+        layout: core::alloc::Layout,
+    ) -> Result<NonNull<[u8]>, core::alloc::AllocError> {
+        if layout.size() < layout.align() {
+            return Err(AllocError);
+        }
+
+        match layout.size() {
+            Generic8::START..=Generic8::END => {
+                Ok(self.kmalloc::<Generic8>().into_u8())
+            }
+            Generic16::START..=Generic16::END => {
+                Ok(self.kmalloc::<Generic16>().into_u8())
+            }
+            Generic32::START..=Generic32::END => {
+                Ok(self.kmalloc::<Generic32>().into_u8())
+            }
+            Generic64::START..=Generic64::END => {
+                Ok(self.kmalloc::<Generic64>().into_u8())
+            }
+            Generic96::START..=Generic96::END => {
+                Ok(self.kmalloc::<Generic96>().into_u8())
+            }
+            Generic128::START..=Generic128::END => {
+                Ok(self.kmalloc::<Generic128>().into_u8())
+            }
+            Generic192::START..=Generic192::END => {
+                Ok(self.kmalloc::<Generic192>().into_u8())
+            }
+            Generic256::START..=Generic256::END => {
+                Ok(self.kmalloc::<Generic256>().into_u8())
+            }
+            Generic512::START..=Generic512::END => {
+                Ok(self.kmalloc::<Generic512>().into_u8())
+            }
+            Generic1024::START..=Generic1024::END => {
+                Ok(self.kmalloc::<Generic1024>().into_u8())
+            }
+            Generic2048::START..=Generic2048::END => {
+                Ok(self.kmalloc::<Generic2048>().into_u8())
+            }
+            Generic4096::START..=Generic4096::END => {
+                Ok(self.kmalloc::<Generic4096>().into_u8())
+            }
+            Generic8192::START..=Generic8192::END => {
+                Ok(self.kmalloc::<Generic8192>().into_u8())
+            }
+            _ => Err(AllocError),
+        }
+    }
+
+    unsafe fn deallocate(
+        &self,
+        ptr: core::ptr::NonNull<u8>,
+        layout: core::alloc::Layout,
+    ) {
+        match layout.size() {
+            Generic8::START..=Generic8::END => {
+                self.kfree::<Generic8>(NonNull::from_u8(ptr))
+            }
+            Generic16::START..=Generic16::END => {
+                self.kfree::<Generic16>(NonNull::from_u8(ptr))
+            }
+            Generic32::START..=Generic32::END => {
+                self.kfree::<Generic32>(NonNull::from_u8(ptr))
+            }
+            Generic64::START..=Generic64::END => {
+                self.kfree::<Generic64>(NonNull::from_u8(ptr))
+            }
+            Generic96::START..=Generic96::END => {
+                self.kfree::<Generic96>(NonNull::from_u8(ptr))
+            }
+            Generic128::START..=Generic128::END => {
+                self.kfree::<Generic128>(NonNull::from_u8(ptr))
+            }
+            Generic192::START..=Generic192::END => {
+                self.kfree::<Generic192>(NonNull::from_u8(ptr))
+            }
+            Generic256::START..=Generic256::END => {
+                self.kfree::<Generic256>(NonNull::from_u8(ptr))
+            }
+            Generic512::START..=Generic512::END => {
+                self.kfree::<Generic512>(NonNull::from_u8(ptr))
+            }
+            Generic1024::START..=Generic1024::END => {
+                self.kfree::<Generic1024>(NonNull::from_u8(ptr))
+            }
+            Generic2048::START..=Generic2048::END => {
+                self.kfree::<Generic2048>(NonNull::from_u8(ptr))
+            }
+            Generic4096::START..=Generic4096::END => {
+                self.kfree::<Generic4096>(NonNull::from_u8(ptr))
+            }
+            Generic8192::START..=Generic8192::END => {
+                self.kfree::<Generic8192>(NonNull::from_u8(ptr))
+            }
+            _ => unreachable!(),
+        }
+    }
+}
 unsafe impl<T: SlabPosition> Send for SlabDescriptor<T> {}
 unsafe impl<T: SlabPosition> Sync for SlabDescriptor<T> {}
 unsafe impl<T: SlabPosition> Send for SlabCache<T> {}
