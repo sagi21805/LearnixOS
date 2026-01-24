637858ea9914ea58636d0108bed8b832a9165d62
sagi <sagi21805@gmail.com>
2026-01-25 00:57:51 +0200

    moved functions from page into buddy

--- kernel/src/memory/allocators/buddy.rs
+++ kernel/src/memory/allocators/buddy.rs
@@ -3,6 +3,7 @@ use core::ptr::{self, NonNull};
 use common::{
     address_types::PhysicalAddress,
     enums::{BUDDY_MAX_ORDER, BuddyOrder, MemoryRegionType},
+    write_volatile,
 };
 use cpu_utils::structures::paging::PageTable;
 
@@ -15,6 +16,15 @@ use crate::memory::{
 pub static mut BUDDY_ALLOCATOR: BuddyAllocator = BuddyAllocator {
     freelist: [const { BuddyPageMeta::default() }; BUDDY_MAX_ORDER],
 };
+
+#[macro_export]
+/// Allocate the amount of pages specified, and return the address
+macro_rules! alloc_pages {
+    ($page_number: expr) => {{
+        use $crate::memory::allocators::buddy::BUDDY_ALLOCATOR;
+        BUDDY_ALLOCATOR.alloc_pages($page_number)
+    }};
+}
 
 pub struct BuddyAllocator {
     freelist: [BuddyPageMeta; BUDDY_MAX_ORDER],
@@ -50,34 +60,49 @@ impl BuddyAllocator {
         &mut self,
         wanted_order: usize,
     ) -> Option<NonNull<UnassignedPage>> {
-        let mut closet_order = ((wanted_order + 1)..BUDDY_MAX_ORDER)
+        let closet_order = ((wanted_order + 1)..BUDDY_MAX_ORDER)
             .find(|i| self.freelist[*i].next.is_some())?;
 
-        let initial_page = unsafe {
-            self.freelist[closet_order]
-                .detach::<Unassigned>()
-                .unwrap()
-                .as_mut()
-        };
+        let initial_page =
+            self.freelist[closet_order].detach::<Unassigned>().unwrap();
 
-        let (mut lhs, mut rhs) = unsafe { initial_page.split() }.unwrap();
-        closet_order -= 1;
-
-        while closet_order != wanted_order {
-            self.freelist[closet_order].attach(rhs);
-
-            let split_ref = unsafe { lhs.as_mut() };
-
-            (lhs, rhs) = unsafe { split_ref.split().unwrap() };
-            closet_order -= 1;
-        }
-
-        self.freelist[closet_order].attach(rhs);
-        Some(lhs)
+        Some(self.split_recursive(
+            initial_page,
+            closet_order,
+            wanted_order,
+        ))
     }
 
-    pub fn merge_until_max(&self, page: NonNull<UnassignedPage>) {
-        todo!()
+    fn split_recursive(
+        &mut self,
+        page: NonNull<UnassignedPage>,
+        current_order: usize,
+        target_order: usize,
+    ) -> NonNull<UnassignedPage> {
+        debug_assert!(
+            target_order < current_order,
+            "Target order cannot be greater then current order"
+        );
+
+        if current_order == target_order {
+            return page;
+        }
+
+        let (lhs, rhs) = unsafe { BuddyAllocator::split(page).unwrap() };
+
+        let next_order = current_order - 1;
+        self.freelist[next_order].attach(rhs);
+
+        become self.split_recursive(lhs, next_order, target_order)
+    }
+
+    /// This function will try to merge a page on the buddy allocator until
+    pub fn merge_recursive(&self, page: NonNull<UnassignedPage>) {
+        if let Some(merged) =
+            unsafe { BuddyAllocator::merge_with_buddy(page) }
+        {
+            become BuddyAllocator::merge_recursive(self, merged);
+        }
     }
 
     pub fn alloc_table(&mut self) -> &'static mut PageTable {
@@ -136,11 +161,104 @@ impl BuddyAllocator {
         }
     }
 }
-#[macro_export]
-/// Allocate the amount of pages specified, and return the address
-macro_rules! alloc_pages {
-    ($page_number: expr) => {{
-        use $crate::memory::allocators::buddy::BUDDY_ALLOCATOR;
-        BUDDY_ALLOCATOR.alloc_pages($page_number)
-    }};
+
+impl BuddyAllocator {
+    /// TODO: Make an unsafe split if relevant
+    ///
+    /// # Safety
+    /// This function does not attach the new references!
+    #[allow(clippy::type_complexity)]
+    unsafe fn split(
+        mut page: NonNull<UnassignedPage>,
+    ) -> Option<(NonNull<UnassignedPage>, NonNull<UnassignedPage>)> {
+        // Reduce it's order to find it's order.
+        let prev_order = BuddyOrder::try_from(
+            unsafe { page.as_ref().meta.buddy.order? } as u8 - 1,
+        )
+        .expect("Page order cannot be reduced");
+
+        write_volatile!(
+            (*page.as_mut().meta.buddy).order,
+            Some(prev_order)
+        );
+
+        let index = unsafe {
+            ((page.as_ref() as *const _ as usize - PAGES.as_ptr().addr())
+                / size_of::<UnassignedPage>())
+                + (1 << prev_order as usize)
+        };
+
+        // Find it's half
+        let mut buddy = unsafe { NonNull::from_mut(&mut PAGES[index]) };
+
+        // Set the order of the buddy.
+        write_volatile!(
+            (*buddy.as_mut().meta.buddy).order,
+            Some(prev_order)
+        );
+
+        Some((page, buddy))
+    }
+
+    /// This function will detach the given page and it's buddy from their
+    /// freelist, increase their and attach to the increased order
+    /// list.
+    unsafe fn merge_with_buddy(
+        page: NonNull<UnassignedPage>,
+    ) -> Option<NonNull<UnassignedPage>> {
+        let buddy = BuddyAllocator::buddy_of(page)?;
+
+        let next_order = BuddyOrder::try_from(unsafe {
+            page.as_ref().meta.buddy.order.unwrap() as u8 + 1
+        })
+        .unwrap();
+
+        BuddyAllocator::detach_from_mid(page);
+        BuddyAllocator::detach_from_mid(buddy);
+
+        // Operate on the page that it's address is lower.
+        let (mut left, mut right) = if page < buddy {
+            (page, buddy)
+        } else {
+            (buddy, page)
+        };
+
+        unsafe {
+            (*left.as_mut().meta.buddy).order = Some(next_order);
+            (*right.as_mut().meta.buddy) = BuddyPageMeta::default();
+        };
+
+        Some(left)
+    }
+
+    fn detach_from_mid(page: NonNull<UnassignedPage>) {
+        let (mut prev, next) = unsafe {
+            let p_ref = page.as_ref();
+            (
+                p_ref.meta.buddy.prev.expect("Page has no prev"),
+                p_ref.meta.buddy.next.expect("Page has no next"),
+            )
+        };
+
+        unsafe { (*prev.as_mut().meta.buddy).next = Some(next) }
+    }
+
+    fn buddy_of(
+        page: NonNull<UnassignedPage>,
+    ) -> Option<NonNull<UnassignedPage>> {
+        let order = unsafe { page.as_ref().meta.buddy.order? };
+        if let BuddyOrder::MAX = order {
+            None
+        } else {
+            unsafe {
+                let buddy_address = page.as_ref() as *const _ as usize
+                    ^ ((1 << order as usize)
+                        * size_of::<UnassignedPage>());
+
+                Some(NonNull::new_unchecked(
+                    buddy_address as *mut UnassignedPage,
+                ))
+            }
+        }
+    }
 }
--- kernel/src/memory/page.rs
+++ kernel/src/memory/page.rs
@@ -1,22 +1,13 @@
-use core::{mem::ManuallyDrop, ptr::NonNull};
+use core::{marker::PhantomData, ptr::NonNull};
 
-use crate::{
-    memory::{
-        allocators::slab::{
-            descriptor::SlabDescriptor, traits::SlabPosition,
-        },
-        memory_map::ParsedMemoryMap,
-        page::meta::{BuddyPageMeta, PageMeta},
-        unassigned::{AssignSlab, UnassignSlab, Unassigned},
-    },
-    println,
+use crate::memory::{
+    allocators::slab::traits::SlabPosition,
+    page::{map::PageMap, meta::PageMeta},
+    unassigned::{AssignSlab, UnassignSlab, Unassigned},
 };
 use common::{
-    address_types::PhysicalAddress,
-    constants::{PAGE_ALLOCATOR_OFFSET, REGULAR_PAGE_SIZE},
-    enums::BuddyOrder,
+    address_types::PhysicalAddress, constants::REGULAR_PAGE_SIZE,
     late_init::LateInit,
-    write_volatile,
 };
 
 pub mod map;
@@ -24,8 +15,12 @@ pub mod meta;
 
 pub type UnassignedPage = Page<Unassigned>;
 
-pub static mut PAGES: LateInit<&'static mut [UnassignedPage]> =
-    LateInit::uninit();
+pub static mut PAGES: LateInit<PageMap> = LateInit::uninit();
+
+pub struct Page<T: 'static + SlabPosition> {
+    pub meta: PageMeta,
+    _phantom: PhantomData<T>,
+}
 
 impl AssignSlab for NonNull<Page<Unassigned>> {
     type Target<Unassigned: SlabPosition> = NonNull<Page<Unassigned>>;
@@ -45,12 +40,14 @@ impl<T: SlabPosition> UnassignSlab for NonNull<Page<T>> {
     }
 }
 
-pub struct Page<T: 'static + SlabPosition> {
-    pub owner: Option<NonNull<SlabDescriptor<T>>>,
-    pub meta: PageMeta,
-}
-
 impl<T: 'static + SlabPosition> Page<T> {
+    pub fn new(meta: PageMeta) -> Page<T> {
+        Page {
+            meta,
+            _phantom: PhantomData::<T>,
+        }
+    }
+
     pub fn physical_address(&self) -> PhysicalAddress {
         let index = (self as *const _ as usize
             - unsafe { PAGES.as_ptr().addr() })
@@ -61,95 +58,7 @@ impl<T: 'static + SlabPosition> Page<T> {
         }
     }
 
-    pub fn get_buddy(&self) -> Option<*mut Page<T>> {
-        let order = unsafe { self.meta.buddy.order? };
-        if let BuddyOrder::MAX = order {
-            None
-        } else {
-            Some(
-                (self as *const _ as usize
-                    ^ ((1 << order as usize)
-                        * size_of::<UnassignedPage>()))
-                    as *mut Page<T>,
-            )
-        }
-    }
-
-    /// TODO: Make an unsafe split if relevant
-    ///
-    /// # Safety
-    /// This function does not attach the new references!
-    #[allow(clippy::type_complexity)]
-    pub unsafe fn split(
-        &mut self,
-    ) -> Option<(NonNull<Page<T>>, NonNull<Page<T>>)> {
-        // Reduce it's order to find it's order.
-
-        let prev_order = BuddyOrder::try_from(
-            unsafe { self.meta.buddy.order? } as u8 - 1,
-        )
-        .unwrap();
-
-        write_volatile!((*self.meta.buddy).order, Some(prev_order));
-
-        let index = ((self as *const _ as usize
-            - unsafe { PAGES.as_ptr().addr() })
-            / size_of::<UnassignedPage>())
-            + (1 << prev_order as usize);
-
-        // Find it's half
-        let mut buddy =
-            unsafe { NonNull::from_mut(&mut PAGES[index]).assign::<T>() };
-
-        // Set the order of the buddy.
-        write_volatile!(
-            (*buddy.as_mut().meta.buddy).order,
-            Some(prev_order)
-        );
-
-        Some((NonNull::from_mut(self), buddy))
-    }
-
-    /// Try to merge this page with it's buddy.
-    ///
-    /// Note: This function should not be recursive
-    pub unsafe fn merge(&self) {
-        todo!("")
-    }
-
     pub const fn index_of_page(address: PhysicalAddress) -> usize {
         address.as_usize() / REGULAR_PAGE_SIZE
-    }
-}
-
-pub fn pages_init(mmap: ParsedMemoryMap) -> usize {
-    let last = mmap.last().unwrap();
-    let last_address = (last.base_address + last.length) as usize;
-    let total_pages = last_address / REGULAR_PAGE_SIZE;
-
-    println!(
-        "Last address: {}, Total Pages: {}, size_of_array: {:x?} Kib",
-        last_address,
-        total_pages,
-        total_pages * size_of::<Page<Unassigned>>() / 1024
-    );
-    unsafe {
-        PAGES.write(core::slice::from_raw_parts_mut(
-            PAGE_ALLOCATOR_OFFSET as *mut UnassignedPage,
-            total_pages,
-        ));
-
-        for p in PAGES.iter_mut() {
-            core::ptr::write_volatile(
-                p as *mut UnassignedPage,
-                UnassignedPage {
-                    meta: PageMeta {
-                        buddy: ManuallyDrop::new(BuddyPageMeta::default()),
-                    },
-                    owner: None,
-                },
-            );
-        }
-        PAGES.as_ptr_range().end as usize
     }
 }
