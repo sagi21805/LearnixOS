ef5fa1fee819ab03036d82ad957939dd5b8143c6
sagi <sagi21805@gmail.com>
2026-01-28 22:25:02 +0200

    used new slab trait, and added a grow function

--- kernel/src/memory/allocators/slab/cache.rs
+++ kernel/src/memory/allocators/slab/cache.rs
@@ -1,11 +1,24 @@
+use core::{num::NonZero, ptr::NonNull};
+
+use common::{address_types::VirtualAddress, enums::PageSize};
 use cpu_utils::structures::paging::PageEntryFlags;
 
-use super::descriptor::SlabDescriptor;
-use super::traits::{SlabCacheConstructor, SlabPosition};
-use crate::memory::allocators::slab::SLAB_ALLOCATOR;
-use crate::memory::allocators::slab::traits::DmaGeneric;
-use crate::memory::unassigned::{AssignSlab, Unassigned};
-use core::ptr::NonNull;
+use crate::memory::{
+    allocators::{
+        extensions::VirtualAddressExt,
+        slab::{
+            SLAB_ALLOCATOR,
+            traits::{Slab, SlabFlags},
+        },
+    },
+    page::{PAGES, UnassignedPage},
+    unassigned::{AssignSlab, UnassignSlab, Unassigned},
+};
+
+use super::{
+    descriptor::SlabDescriptor,
+    traits::{SlabCacheConstructor, SlabPosition},
+};
 
 #[derive(Clone, Debug)]
 pub struct SlabCache<T: 'static + Sized + SlabPosition> {
@@ -16,13 +29,53 @@ pub struct SlabCache<T: 'static + Sized + SlabPosition> {
     pub full: Option<NonNull<SlabDescriptor<T>>>,
 }
 
-impl<T: SlabPosition> SlabCache<T> {
+impl<T: Slab> SlabCache<T> {
     pub fn as_unassigned(&self) -> &SlabCache<Unassigned> {
+        todo!("Change to trait implementation");
         unsafe { &*(self as *const _ as *const SlabCache<Unassigned>) }
     }
 
     pub fn as_unassigned_mut(&mut self) -> &mut SlabCache<Unassigned> {
+        todo!("Change to trait implementation");
         unsafe { &mut *(self as *mut _ as *mut SlabCache<Unassigned>) }
+    }
+
+    /// Allocate a new slab descriptor, attaches it to the free slab list,
+    /// and initialize it's page.
+    pub fn grow(&self) -> NonNull<SlabDescriptor<T>> {
+        // Allocate a new slab descriptor for this slab
+        let mut slab = unsafe {
+            SLAB_ALLOCATOR
+                .kmalloc::<SlabDescriptor<Unassigned>>()
+                .assign::<T>()
+        };
+
+        unsafe {
+            *slab.as_mut() =
+                SlabDescriptor::<T>::new(self.buddy_order, None)
+        }
+
+        let slab_address: VirtualAddress =
+            unsafe { slab.as_ref().objects.as_ptr().addr().into() };
+
+        slab_address
+            .set_flags(self.pflags, PageSize::Regular, unsafe {
+                NonZero::<usize>::new_unchecked(1 << self.buddy_order)
+            })
+            .unwrap();
+
+        let slab_page = unsafe {
+            &mut PAGES[UnassignedPage::index_of_page(slab_address)]
+        };
+
+        // Set owner and freelist.
+        unsafe {
+            (*slab_page.meta.slab).freelist = slab.as_unassigned();
+            (*slab_page.meta.slab).owner =
+                NonNull::from_ref(self.as_unassigned());
+        };
+
+        slab
     }
 
     pub fn alloc(&mut self) -> NonNull<T> {
@@ -61,59 +114,19 @@ impl<T: SlabPosition> SlabCache<T> {
 }
 
 impl SlabCache<Unassigned> {
-    pub fn assign<T: SlabPosition>(&self) -> NonNull<SlabCache<T>> {
+    pub fn assign<T: Slab>(&self) -> NonNull<SlabCache<T>> {
         unsafe {
             NonNull::new_unchecked(self as *const _ as *mut SlabCache<T>)
         }
     }
 }
 
-impl<T: SlabPosition> SlabCacheConstructor for SlabCache<T> {
+impl<T: Slab> SlabCacheConstructor for SlabCache<T> {
     default fn new(buddy_order: usize) -> SlabCache<T> {
-        const PFLAGS: PageEntryFlags =
-            PageEntryFlags::regular_io_page_flags();
-
-        let mut free = unsafe {
-            SLAB_ALLOCATOR
-                .slab_of::<SlabDescriptor<Unassigned>>()
-                .as_mut()
-                .alloc()
-        };
-
-        unsafe {
-            *free.as_mut() = SlabDescriptor::new(buddy_order, PFLAGS, None)
-        }
-
         SlabCache {
             buddy_order,
-            pflags: PFLAGS,
-            free: Some(free.assign::<T>()),
-            partial: None,
-            full: None,
-        }
-    }
-}
-
-impl<T: SlabPosition + DmaGeneric> SlabCacheConstructor for SlabCache<T> {
-    fn new(buddy_order: usize) -> Self {
-        const PFLAGS: PageEntryFlags =
-            PageEntryFlags::regular_io_page_flags();
-
-        let mut free = unsafe {
-            SLAB_ALLOCATOR
-                .slab_of::<SlabDescriptor<Unassigned>>()
-                .as_mut()
-                .alloc()
-        };
-
-        unsafe {
-            *free.as_mut() = SlabDescriptor::new(buddy_order, PFLAGS, None)
-        }
-
-        SlabCache {
-            buddy_order,
-            pflags: PFLAGS,
-            free: Some(free.assign::<T>()),
+            pflags: T::PFLAGS,
+            free: None,
             partial: None,
             full: None,
         }
@@ -125,7 +138,7 @@ impl SlabCacheConstructor for SlabCache<SlabDescriptor<Unassigned>> {
         let partial = SlabDescriptor::<SlabDescriptor<Unassigned>>::initial_descriptor(buddy_order);
         SlabCache {
             buddy_order,
-            pflags: PageEntryFlags::regular_page_flags(),
+            pflags: SlabDescriptor::<Unassigned>::PFLAGS,
             free: None,
             partial: Some(partial),
             full: None,
