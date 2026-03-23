f70fde1c8250fe993a353b55df189bc8bb686ab7
sagi <sagi21805@gmail.com>
2026-03-23 14:36:20 +0200

    initial implementation using special typestate doubly linked list

--- crates/memory/buddy/src/lib.rs
+++ crates/memory/buddy/src/lib.rs
@@ -4,31 +4,49 @@
 #![feature(explicit_tail_calls)]
 pub mod meta;
 
-use core::ptr::{self, NonNull};
+use core::{marker::PhantomData, ptr::{self, NonNull}};
+
+use x86::structures::paging::PageTable;
 
 use common::{
     address_types::PhysicalAddress,
-    enums::{BUDDY_MAX_ORDER, BuddyOrder, MemoryRegionType},
-    write_volatile,
+    enums::{BUDDY_MAX_ORDER, BuddyOrder},
 };
-use x86::{memory_map::ParsedMemoryMap, structures::paging::PageTable};
 
-use crate::meta::{BuddyPage, BuddyPageMeta};
+use crate::meta::{BuddyArena, BuddyBlock, BuddyMeta, Dummy};
 
-#[macro_export]
-/// Allocate the amount of pages specified, and return the address
-macro_rules! alloc_pages {
-    ($page_number: expr) => {{
-        use $crate::memory::allocators::buddy::BUDDY_ALLOCATOR;
-        BUDDY_ALLOCATOR.alloc_pages($page_number)
-    }};
+pub struct BuddyAllocator<Arena, Block>
+where
+    Block: BuddyBlock,
+    Arena: BuddyArena<Block>,
+{
+    arena: NonNull<Arena>,
+    freelist: [BuddyMeta<Dummy>; BUDDY_MAX_ORDER],
+    _block: PhantomData<Block>
 }
 
-pub struct BuddyAllocator<T: BuddyPage> {
-    freelist: [BuddyPageMeta<T>; BUDDY_MAX_ORDER],
+impl<Arena, Block> BuddyAllocator<Arena, Block>
+where
+    Arena: BuddyArena<Block>,
+    Block: BuddyBlock,
+{
+
+    pub fn init(&'static mut self, arena: NonNull<Arena>) {
+        for block in unsafe { arena.as_ref().iter() } {
+            let Some(order) = unsafe {  block.as_ref() }.meta().order else {
+                continue;
+            };
+
+            self.freelist[order as usize].attach(NonNull::from_ref(unsafe { block.as_ref().meta() }));
+        }
+    }
 }
 
-impl<T: BuddyPage> BuddyAllocator<T> {
+impl<Arena, Block> BuddyAllocator<Arena, Block>
+where
+    Arena: BuddyArena<Block>,
+    Block: BuddyBlock,
+{
     pub fn alloc_pages(&mut self, num_pages: usize) -> PhysicalAddress {
         assert!(
             num_pages <= (1 << BuddyOrder::MAX as usize),
@@ -40,12 +58,12 @@ impl<T: BuddyPage> BuddyAllocator<T> {
             - num_pages.next_power_of_two().leading_zeros())
             as usize;
 
-        let page = self.freelist[order].detach().unwrap_or_else(|| {
-            self.split_until(order)
-                .expect("Out of memory, swap is not implemented")
+        let page = self.freelist[order].next.unwrap_or_else(|| {
+            NonNull::from_ref(unsafe { self.split_until(order)
+                .expect("Out of memory, swap is not implemented").as_ref().meta() })
         });
-        todo!();
-        // unsafe { page.as_ref().physical_address() }
+
+        unsafe { self.arena.as_ref().address_of(page) }
     }
 
     // pub fn free_pages(&self, address: usize) {
@@ -57,11 +75,10 @@ impl<T: BuddyPage> BuddyAllocator<T> {
     pub fn split_until(
         &mut self,
         wanted_order: usize,
-    ) -> Option<NonNull<T>> {
-        let closet_order = ((wanted_order + 1)..BUDDY_MAX_ORDER)
-            .find(|i| self.freelist[*i].next.is_some())?;
-
-        let initial_page = self.freelist[closet_order].detach().unwrap();
+    ) -> Option<NonNull<Block>> {
+        let (closet_order, initial_page) = ((wanted_order + 1)
+            ..BUDDY_MAX_ORDER)
+            .find_map(|i| Some((i, self.freelist[i].next?)))?;
 
         Some(self.split_recursive(
             initial_page,
@@ -72,10 +89,10 @@ impl<T: BuddyPage> BuddyAllocator<T> {
 
     fn split_recursive(
         &mut self,
-        page: NonNull<T>,
+        page: NonNull<Block>,
         current_order: usize,
         target_order: usize,
-    ) -> NonNull<T> {
+    ) -> NonNull<Block> {
         debug_assert!(
             target_order < current_order,
             "Target order cannot be greater then current order"
@@ -85,16 +102,40 @@ impl<T: BuddyPage> BuddyAllocator<T> {
             return page;
         }
 
-        let (lhs, rhs) = unsafe { BuddyAllocator::split(page).unwrap() };
+        let (lhs, rhs) = unsafe { self.arena.as_ref().split(page) };
 
         let next_order = current_order - 1;
-        self.freelist[next_order].attach(rhs);
+        match self.freelist[next_order] {
+            Some(mut block) => {
+                unsafe { block.as_mut().meta_mut().attach(rhs) };
+            }
+            None => self.freelist[next_order] = Some(rhs),
+        }
 
         become self.split_recursive(lhs, next_order, target_order)
     }
 
-    /// This function will try to merge a page on the buddy allocator until
-    pub fn merge_recursive(&self, page: NonNull<T>) {
+    /// This function will try to merge a page with it's buddy, until it
+    /// cannot be merged anymore.
+    pub fn merge_recursive(&self, page: NonNull<Block>) -> NonNull<Block> {
+        let buddy = match unsafe { self.arena.as_ref().buddy_of(page) } {
+            Ok(buddy) => buddy,
+            Err(BuddyError::MaxOrder) => {
+                self.freelist[]
+            }
+        }
+
+
+
+        let (mut left, mut right) = (page, buddy);
+        unsafe {
+            if self.arena.as_ref().address_of(left)
+                > self.arena.as_ref().address_of(right)
+            {
+                core::mem::swap(&mut left, &mut right);
+            }
+        }
+
         if let Some(merged) =
             unsafe { BuddyAllocator::merge_with_buddy(page) }
         {
@@ -110,149 +151,6 @@ impl<T: BuddyPage> BuddyAllocator<T> {
                 PageTable::empty(),
             );
             address.as_non_null::<PageTable>()
-        }
-    }
-
-    /// The code_end number should be the end address of the code.
-    ///
-    /// This function will not put in the free list pages that hold
-    /// addresses from 0->code_end
-    pub fn init(&'static mut self, map: ParsedMemoryMap, code_end: usize) {
-        //     for area in map
-        //         .iter()
-        //         .filter(|a| a.region_type == MemoryRegionType::Usable)
-        //     {
-        //         let mut start =
-        //             T::index_of((area.base_address as usize).into());
-        //         let end = UnassignedPage::index_of(
-        //             ((area.base_address + area.length) as usize).into(),
-        //         );
-
-        //         let mut prev = None;
-
-        //         while start < end {
-        //             let largest_order = BuddyOrder::try_from(
-        //                 ((end - start).ilog2().min(BuddyOrder::MAX as
-        // u32))                     as u8,
-        //             )
-        //             .unwrap();
-
-        //             let curr = unsafe { &mut PAGES[start] };
-        //             let next = unsafe {
-        //                 &mut PAGES[start + ((1 << largest_order as
-        // usize) - 1)]             };
-        //             unsafe {
-        //                 (*curr.meta.buddy).next =
-        //                     Some(NonNull::from_mut(next));
-        //                 (*curr.meta.buddy).prev = prev;
-        //                 (*curr.meta.buddy).order = Some(largest_order);
-        //             }
-        //             prev = Some(NonNull::from_mut(curr));
-
-        //             self.freelist[largest_order as usize]
-        //                 .attach(NonNull::from_mut(curr));
-
-        //             start += 1 << largest_order as usize;
-        //         }
-        //     }
-    }
-}
-
-impl<T: BuddyPage> BuddyAllocator<T> {
-    /// TODO: Make an unsafe split if relevant
-    /// TODO: Understand how to use the buddy of the page instead of
-    /// splitting using the global page array
-    ///
-    /// # Safety
-    /// This function does not attach the new references!
-    #[allow(clippy::type_complexity)]
-    unsafe fn split(
-        mut page: NonNull<T>,
-    ) -> Option<(NonNull<T>, NonNull<T>)> {
-        // // Reduce it's order to find it's order.
-        // let prev_order = BuddyOrder::try_from(
-        //     unsafe { page.as_ref().meta().order? } as u8 - 1,
-        // )
-        // .expect("Page order cannot be reduced");
-
-        // write_volatile!(page.as_mut().meta().order, Some(prev_order));
-
-        // let index = unsafe {
-        //     ((page.as_ref() as *const _ as usize -
-        // PAGES.as_ptr().addr())         / size_of::<T>())
-        //         + (1 << prev_order as usize)
-        // };
-
-        // // Find it's half
-        // let mut buddy = unsafe { NonNull::from_mut(&mut PAGES[index]) };
-
-        // // Set the order of the buddy.
-        // write_volatile!(
-        //     (*buddy.as_mut().meta.buddy).order,
-        //     Some(prev_order)
-        // );
-
-        // Some((page, buddy))
-        todo!()
-    }
-
-    /// This function will detach the given page and it's buddy from their
-    /// freelist, increase their and attach to the increased order
-    /// list.
-    unsafe fn merge_with_buddy(
-        mut page: NonNull<T>,
-    ) -> Option<NonNull<T>> {
-        let buddy = BuddyAllocator::buddy_of(page)?;
-
-        let next_order = BuddyOrder::try_from(unsafe {
-            page.as_mut().meta().order.unwrap() as u8 + 1
-        })
-        .unwrap();
-
-        BuddyAllocator::detach_from_mid(page);
-        BuddyAllocator::detach_from_mid(buddy);
-
-        // Operate on the page that it's address is lower.
-        let (mut left, mut right) = if page < buddy {
-            (page, buddy)
-        } else {
-            (buddy, page)
-        };
-
-        unsafe {
-            left.as_mut().meta().order = Some(next_order);
-            *right.as_mut().meta() = BuddyPageMeta::default();
-        };
-
-        Some(left)
-    }
-
-    // TODO: This function will probably fail, should change that the head
-    // of the page list is static and the list starts from the second
-    // node, and then this would work
-    fn detach_from_mid(mut page: NonNull<T>) {
-        let (mut prev, next) = unsafe {
-            let p_ref = page.as_mut();
-            (
-                p_ref.meta().prev.expect("Page has no prev"),
-                p_ref.meta().next.expect("Page has no next"),
-            )
-        };
-
-        unsafe { prev.as_mut().meta().next = Some(next) }
-    }
-
-    fn buddy_of(mut page: NonNull<T>) -> Option<NonNull<T>> {
-        let order = unsafe { page.as_mut().meta().order? };
-        if let BuddyOrder::MAX = order {
-            None
-        } else {
-            unsafe {
-                let buddy_address = page.as_ref() as *const _ as usize
-                    ^ ((1 << order as usize) * size_of::<T>());
-
-                Some(NonNull::new_unchecked(buddy_address as *mut T))
-            }
         }
     }
 }
--- crates/memory/buddy/src/meta.rs
+++ crates/memory/buddy/src/meta.rs
@@ -1,48 +1,107 @@
-use core::ptr::NonNull;
+use core::{marker::PhantomData, ptr::NonNull};
 
-use common::enums::BuddyOrder;
+use common::{
+    address_types::PhysicalAddress, constants::REGULAR_PAGE_SIZE,
+    enums::BuddyOrder,
+};
 
-pub trait BuddyPage {
-    fn meta(&mut self) -> &mut BuddyPageMeta<Self>;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum BuddyError {
+    #[error("Cannot find a buddy for a block that is MAX_ORDER")]
+    MaxOrder,
 }
 
+mod private {
+    pub trait Seald {}
+}
+
+pub struct Dummy;
+pub struct Real;
+
+pub trait MetaState: private::Seald {
+    type Prev<Block: Sized>;
+}
+impl private::Seald for Dummy {}
+impl MetaState for Dummy {
+    type Prev<Block: Sized> = ();
+}
+impl private::Seald for Real {}
+impl MetaState for Real {
+    type Prev<Block: Sized> = NonNull<Block>;
+}
+
+pub trait BuddyBlock: Sized {
+    fn meta(&self) -> &BuddyMeta<Real>;
+
+    fn meta_mut(&mut self) -> &mut BuddyMeta<Real>;
+
+    fn from_meta(meta: NonNull<BuddyMeta<Real>>) -> NonNull<Self>;
+}
+// TODO: MOVE TO BUDDY META REAL
+// fn is_allocated(&self) -> bool {
+//     self.meta().next.is_none() && self.meta().prev.is_none()
+// }
 #[derive(Debug)]
-pub struct BuddyPageMeta<T: BuddyPage + ?Sized> {
-    pub next: Option<NonNull<T>>,
-    pub prev: Option<NonNull<T>>,
+pub struct BuddyMeta<State: MetaState> {
+    pub next: Option<NonNull<BuddyMeta<Real>>>,
+    pub prev: State::Prev<BuddyMeta<State>>,
     pub order: Option<BuddyOrder>,
+    _state: PhantomData<State>,
 }
 
-impl<T: BuddyPage> const Default for BuddyPageMeta<T> {
+impl const Default for BuddyMeta<Dummy> {
     fn default() -> Self {
         Self {
             next: None,
-            prev: None,
+            prev: (),
             order: None,
+            _state: PhantomData,
         }
     }
 }
 
-impl<T: BuddyPage> BuddyPageMeta<T> {
-    pub fn detach(&mut self) -> Option<NonNull<T>> {
-        let mut detached = self.next?; // None if there is no page to detach
-
-        self.next = unsafe { detached.as_mut().meta().next };
-
+impl<State: MetaState> BuddyMeta<State> {
+    pub fn attach(&mut self, mut p: NonNull<BuddyMeta<Real>>) {
+        unsafe { p.as_mut().next = self.next };
         if let Some(mut next) = self.next {
-            unsafe { next.as_mut().meta().prev = None }
+            unsafe { next.as_mut().prev = p };
         }
-
-        Some(detached)
-    }
-
-    pub fn attach(&mut self, mut p: NonNull<T>) {
-        unsafe { p.as_mut().meta().next = self.next };
-
-        if let Some(mut next) = self.next {
-            unsafe { next.as_mut().meta().prev = Some(p) };
-        }
-
         self.next = Some(p)
     }
+}
+
+impl BuddyMeta<Real> {
+    /// Detaches self from the list.
+    pub fn detach(&mut self) -> NonNull<BuddyMeta<Real>> {
+        unsafe { self.prev.as_mut().next = self.next }
+
+        if let Some(mut next) = self.next {
+            unsafe { next.as_mut().prev = self.prev };
+        }
+
+        NonNull::from_mut(self)
+    }
+}
+
+pub trait BuddyArena<Block: BuddyBlock> {
+    fn iter(&self) -> impl Iterator<Item = NonNull<Block>>;
+
+    fn buddy_of(
+        &self,
+        block: NonNull<Block>,
+    ) -> Result<NonNull<Block>, BuddyError>;
+
+    fn address_of(
+        &self,
+        block: NonNull<BuddyMeta<Real>>,
+    ) -> PhysicalAddress;
+
+    fn split(
+        &self,
+        block: NonNull<Block>,
+    ) -> (NonNull<Block>, NonNull<Block>);
+
+    fn merge(&self, block: NonNull<Block>, buddy: NonNull<Block>);
 }
