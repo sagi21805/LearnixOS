5376c0cb364d414d216af759226e121dc82ddf31
sagi21805 <sagi21805@gmail.com>
2026-05-21 12:51:17 +0300

    Compacted code and made more readable

--- crates/macros/src/bitfields.rs
+++ crates/macros/src/bitfields.rs
@@ -3,19 +3,20 @@ mod flag_attr;
 mod utils;
 
 use proc_macro2::TokenStream as TokenStream2;
-use quote::{format_ident, quote};
-use syn::{Attribute, Ident, ItemStruct, TypePath};
+use quote::{ToTokens, format_ident, quote};
+use syn::{Attribute, Ident, ItemStruct, TypePath, Visibility};
 
 use crate::bitfields::bitfield::BitField;
 
-pub struct Bitflags<'a> {
+pub struct BitFields<'a> {
     attrs: &'a Vec<Attribute>,
+    vis: &'a Visibility,
     struct_name: &'a Ident,
     struct_type: Box<TypePath>,
     fields: Vec<BitField<'a>>,
 }
 
-impl<'a> TryFrom<&'a ItemStruct> for Bitflags<'a> {
+impl<'a> TryFrom<&'a ItemStruct> for BitFields<'a> {
     type Error = syn::Error;
 
     fn try_from(value: &'a ItemStruct) -> Result<Self, Self::Error> {
@@ -30,8 +31,9 @@ impl<'a> TryFrom<&'a ItemStruct> for Bitflags<'a> {
             })
             .collect::<syn::Result<Vec<_>>>()?;
 
-        Ok(Bitflags {
+        Ok(BitFields {
             attrs: &value.attrs,
+            vis: &value.vis,
             struct_name: &value.ident,
             struct_type: utils::type_from_size(offset)?,
             fields,
@@ -39,37 +41,11 @@ impl<'a> TryFrom<&'a ItemStruct> for Bitflags<'a> {
     }
 }
 
-impl<'a> Bitflags<'a> {
-    fn fn_build(&self, field: &'a BitField) -> TokenStream2 {
-        if !field.attr.permissions.write {
-            return TokenStream2::new();
-        }
-
-        let BitField {
-            attr,
-            vis,
-            name,
-            ty,
-            offset,
-            doc_attrs,
-            ..
-        } = field;
-
+impl<'a> BitFields<'a> {
+    fn checks(&self, field: &'a BitField) -> TokenStream2 {
+        let offset = field.offset;
+        let size = field.ty.size;
         let struct_type = &self.struct_type;
-        let size = ty.size;
-        let repr_ty = if attr.dont_shift {
-            struct_type
-        } else {
-            &ty.repr_ty
-        };
-        let ty = attr.flag_type.as_ref().unwrap_or(repr_ty);
-
-        // We literly don't shift
-        let shift = if attr.dont_shift {
-            quote! {}
-        } else {
-            quote! { << #offset }
-        };
 
         let mut checks = quote! {
             debug_assert!(
@@ -80,7 +56,7 @@ impl<'a> Bitflags<'a> {
             );
         };
 
-        if attr.dont_shift {
+        if field.attr.dont_shift {
             checks.extend(quote! {
                 debug_assert!(
                     v & !((((1 << #size) - 1) as #struct_type) << #offset) == 0,
@@ -88,6 +64,51 @@ impl<'a> Bitflags<'a> {
                 );
             });
         }
+
+        checks
+    }
+
+    /// Returns the types needed to represent the item.
+    /// (The actual type, uint repr type, struct type)
+    fn types(
+        &self,
+        field: &'a BitField,
+    ) -> (&TypePath, &TypePath, &TypePath) {
+        let repr_ty = if field.attr.dont_shift {
+            &*self.struct_type
+        } else {
+            field.ty.repr_ty.as_ref()
+        };
+        let ty = field.attr.flag_type.as_deref().unwrap_or(repr_ty);
+        (ty, repr_ty, self.struct_type.as_ref())
+    }
+
+    fn shift(&self, field: &'a BitField) -> TokenStream2 {
+        let offset = field.offset;
+
+        // We literly don't shift
+        if field.attr.dont_shift {
+            quote! {}
+        } else {
+            quote! { << #offset }
+        }
+    }
+
+    fn fn_build(&self, field: &'a BitField) -> TokenStream2 {
+        if !field.attr.permissions.write {
+            return TokenStream2::new();
+        }
+
+        let BitField {
+            vis,
+            name,
+            doc_attrs,
+            ..
+        } = field;
+
+        let (ty, repr_ty, struct_type) = self.types(field);
+        let checks = self.checks(field);
+        let shift = self.shift(field);
 
         quote! {
             #(#doc_attrs)*
@@ -111,7 +132,6 @@ impl<'a> Bitflags<'a> {
         }
 
         let BitField {
-            attr,
             vis,
             name,
             ty,
@@ -120,23 +140,10 @@ impl<'a> Bitflags<'a> {
             ..
         } = field;
 
-        let struct_type = &self.struct_type;
         let size = ty.size;
-        let repr_ty = if attr.dont_shift {
-            struct_type
-        } else {
-            &ty.repr_ty
-        };
-        let ty = attr.flag_type.as_ref().unwrap_or(repr_ty);
+        let (ty, repr_ty, struct_type) = self.types(field);
+        let shift = self.shift(field);
 
-        // We literly don't shift
-        let shift = if attr.dont_shift {
-            quote! {}
-        } else {
-            quote! { >> #offset }
-        };
-
-        eprintln!("\n\n\n {:?} \n\n\n", ty.path.get_ident());
         let fn_name = if ty.path.get_ident().is_some_and(|i| i == "bool") {
             format_ident!("is_{}", name)
         } else {
@@ -168,7 +175,6 @@ impl<'a> Bitflags<'a> {
         }
 
         let BitField {
-            attr,
             vis,
             name,
             ty,
@@ -178,39 +184,10 @@ impl<'a> Bitflags<'a> {
         } = field;
 
         let fn_name = format_ident!("set_{}", name);
-        let struct_type = &self.struct_type;
         let size = ty.size;
-        let repr_ty = if attr.dont_shift {
-            struct_type
-        } else {
-            &ty.repr_ty
-        };
-        let ty = attr.flag_type.as_ref().unwrap_or(repr_ty);
-
-        // We literly don't shift
-        let shift = if attr.dont_shift {
-            quote! {}
-        } else {
-            quote! { << #offset }
-        };
-
-        let mut checks = quote! {
-            debug_assert!(
-                (
-                    (v as #struct_type) >> #offset
-                ) < (1 << #size) as #struct_type,
-                "Value is too large for this bitfield"
-            );
-        };
-
-        if attr.dont_shift {
-            checks.extend(quote! {
-                debug_assert!(
-                    v & !((((1 << #size) - 1) as #struct_type) << #offset) == 0,
-                    "Value overrides flags on positions that are not in bounds of flag",
-                );
-            });
-        }
+        let (ty, repr_ty, struct_type) = self.types(field);
+        let shift = self.shift(field);
+        let checks = self.checks(field);
 
         quote! {
             #(#doc_attrs)*
@@ -224,10 +201,10 @@ impl<'a> Bitflags<'a> {
 
                 unsafe {
                     let addr = self as *const _ as *mut #struct_type;
-                    let val = core::ptr::read_volatile(addr);
+                    let val = ::core::ptr::read_volatile(addr);
                     let cleared = val & !(((1 << #size) - 1) << #offset);
                     let new = cleared | ((#repr_ty::try_from(v).unwrap() as #struct_type) #shift);
-                    core::ptr::write_volatile(addr, new);
+                    ::core::ptr::write_volatile(addr, new);
                 }
             }
         }
@@ -239,7 +216,6 @@ impl<'a> Bitflags<'a> {
         };
 
         let BitField {
-            attr,
             vis,
             name,
             ty,
@@ -248,43 +224,16 @@ impl<'a> Bitflags<'a> {
             ..
         } = field;
         let fn_name = format_ident!("clear_{}", name);
-        let struct_type = &self.struct_type;
-        let repr_ty = if attr.dont_shift {
-            &ty.repr_ty
-        } else {
-            struct_type
-        };
         let size = ty.size;
-
-        let mut checks = quote! {
-            debug_assert!(
-                (
-                    (v as #struct_type) >> #offset
-                ) < (1 << #size) as #struct_type,
-                "Value is too large for this bitfield"
-            );
-        };
-
-        if attr.dont_shift {
-            checks.extend(quote! {
-                debug_assert!(
-                    v & !((((1 << #size) - 1) as #struct_type) << #offset) == 0,
-                    "Value overrides flags on positions that are not in bounds of flag",
-                );
-            });
-        }
-
-        let shift = if attr.dont_shift {
-            quote! {}
-        } else {
-            quote! { << #offset }
-        };
+        let (_, repr_ty, struct_type) = self.types(field);
+        let checks = self.checks(field);
+        let shift = self.shift(field);
 
         quote! {
             #(#doc_attrs)*
             #[inline]
             #vis fn #fn_name(&mut self) {
-                let v = #repr_ty::try_from(v)
+                let v = #repr_ty::try_from(#clear_val)
                     .ok()
                     .expect("Can't convery value 'v' into the struct type");
 
@@ -292,10 +241,10 @@ impl<'a> Bitflags<'a> {
 
                 unsafe {
                     let addr = self as *const _ as *mut #struct_type;
-                    let val = core::ptr::read_volatile(addr);
+                    let val = ::core::ptr::read_volatile(addr);
                     let cleared = val & !(((1 << #size) - 1) << #offset);
                     let new = cleared | ((#clear_val as #struct_type) #shift);
-                    core::ptr::write_volatile(addr, new);
+                    ::core::ptr::write_volatile(addr, new);
                 }
             }
         }
@@ -325,8 +274,8 @@ impl<'a> Bitflags<'a> {
             .collect();
 
         quote! {
-            impl core::fmt::Debug for #struct_name {
-                fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            impl ::core::fmt::Debug for #struct_name {
+                fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> core::fmt::Result {
                     f.debug_struct(stringify!(#struct_name))
                         #(.field(#field_args))*
                         .finish()
@@ -336,13 +285,13 @@ impl<'a> Bitflags<'a> {
     }
 
     fn from_impl(&self) -> TokenStream2 {
-        let Bitflags {
+        let BitFields {
             struct_name,
             struct_type,
             ..
         } = self;
         quote! {
-            impl const From<#struct_type> for #struct_name {
+            impl const ::core::convert::From<#struct_type> for #struct_name {
                 fn from(value: #struct_type) -> Self {
                     #struct_name(value)
                 }
@@ -352,13 +301,13 @@ impl<'a> Bitflags<'a> {
 
     // impls from for the other type instead of into.
     fn into_impl(&self) -> TokenStream2 {
-        let Bitflags {
+        let BitFields {
             struct_name,
             struct_type,
             ..
         } = self;
         quote! {
-            impl const From<#struct_name> for #struct_type {
+            impl const ::core::convert::From<#struct_name> for #struct_type {
                 fn from(value: #struct_name) -> #struct_type {
                     value.0
                 }
@@ -367,80 +316,54 @@ impl<'a> Bitflags<'a> {
     }
 }
 
-pub fn bitfields_impl(s: ItemStruct) -> syn::Result<TokenStream2> {
-    let bitfield @ Bitflags {
-        attrs,
-        struct_name,
-        struct_type,
-        ..
-    } = &Bitflags::try_from(&s)?;
+impl<'a> ToTokens for BitFields<'a> {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        let BitFields {
+            attrs,
+            vis,
+            struct_name,
+            struct_type,
+            ..
+        } = self;
 
-    let vis = &s.vis;
+        let methods = self.fields.iter().map(|f| {
+            let read = self.fn_read(f);
+            let write = self.fn_write(f);
+            let clear = self.fn_clear(f);
+            let build = self.fn_build(f);
+            quote! { #read #write #clear #build }
+        });
 
-    let methods = bitfield.fields.iter().map(|b| {
-        let read = bitfield.fn_read(b);
-        let write = bitfield.fn_write(b);
-        let clear = bitfield.fn_clear(b);
-        let build = bitfield.fn_build(b);
-        quote! { #read #write #clear #build }
-    });
+        let debug_impl = self.debug_impl();
+        let from_impl = self.from_impl();
+        let into_impl = self.into_impl();
 
-    let debug_impl = bitfield.debug_impl();
-    let from_impl = bitfield.from_impl();
-    let into_impl = bitfield.into_impl();
+        tokens.extend(quote! {
+            #(#attrs)*
+            #[repr(transparent)]
+            #[derive(Copy, Clone)]
+            #vis struct #struct_name(#struct_type);
 
-    Ok(quote! {
+            impl #struct_name {
+                #[inline]
+                pub fn new() -> Self {
+                    Self(0)
+                }
 
-        #(#attrs)*
-        #[repr(transparent)]
-        #[derive(Copy, Clone)]
-        #vis struct #struct_name(#struct_type);
-
-        impl #struct_name {
-            #[inline]
-            pub fn new() -> Self {
-                Self(0)
+                #(#methods)*
             }
 
-            #(#methods)*
-        }
-
-        impl const Default for #struct_name {
-            fn default() -> Self {
-                Self(0)
+            impl const Default for #struct_name {
+                fn default() -> Self {
+                    Self(0)
+                }
             }
-        }
 
-        #from_impl
+            #from_impl
 
-        #into_impl
+            #into_impl
 
-        #debug_impl
-    })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_macro() {
-        let example = quote! {
-            #[bitfields]
-            pub struct MyFlags {
-                #[flag(rwc(40), flag_type = Some::Type)]
-                a: B3,
-
-                #[flag(rw)]
-                b: B1,
-
-                #[flag(rw)]
-                c: B1,
-            }
-        };
-
-        let input = syn::parse2(example).unwrap();
-        let output_tokens = bitfields_impl(input).unwrap();
-        println!("{:#?}", output_tokens);
+            #debug_impl
+        });
     }
 }
