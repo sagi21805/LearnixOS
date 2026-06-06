d9034a93e4a94ecb2a8228e7967819a954335de7
sagi21805 <sagi21805@gmail.com>
2026-06-06 19:50:48 +0300

    define time handler

--- kernel/src/timer.rs
+++ kernel/src/timer.rs
@@ -1,12 +1,11 @@
-use crate::structures::interrupt_descriptor_table::InterruptStackFrame;
 use common::enums::CascadedPicInterruptLine;
+use x86::structures::interrupt_descriptor_table::InterruptStackFrame;
 
-use crate::pic8259::PIC;
+use crate::PIC;
 
 pub extern "x86-interrupt" fn timer_handler(
     _stack_frame: InterruptStackFrame,
 ) {
-    // print!(".");
     unsafe {
         PIC.end_of_interrupt(CascadedPicInterruptLine::Timer);
     }
