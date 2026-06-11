1c267253f525394e3cb140cb5ee1ee9f11350e77
sagi21805 <sagi21805@gmail.com>
2026-06-11 21:58:19 +0300

    Add cool test that can run on the host computer.

--- crates/drivers/vga-display/src/lib.rs
+++ crates/drivers/vga-display/src/lib.rs
@@ -5,6 +5,7 @@
 #![feature(ascii_char_variants)]
 #![feature(const_convert)]
 #![feature(const_result_trait_fn)]
+#![feature(unsafe_cell_access)]
 #![allow(static_mut_refs)]
 pub mod advanced_writer;
 pub mod color_code;
@@ -117,4 +118,64 @@ macro_rules! okprintln {
         $crate::print!("]: ");
         $crate::println!($fmt $(, $arg)* ; color = $color);
     }};
+}
+
+#[cfg(test)]
+extern crate std;
+
+#[cfg(test)]
+mod tests {
+    use ::core::fmt::Write;
+    use core::cell::UnsafeCell;
+    use std::ascii::Char;
+    use std::boxed::Box;
+    use std::vec::Vec;
+
+    use crate::{
+        advanced_writer::AdvancedWriter,
+        color_code::ColorCode,
+        generic_writer::{GenericWriter, Writer},
+        screen_char::ScreenChar,
+    };
+
+    static mut backing: UnsafeCell<[ScreenChar; 80 * 25]> =
+        UnsafeCell::new([ScreenChar::default(); 80 * 25]);
+
+    #[test]
+    fn it_works() {
+        let mut writer =
+            UnsafeCell::new(AdvancedWriter::<80, 25>::default());
+        unsafe {
+            writer.as_mut_unchecked().backing = backing.as_mut_unchecked();
+        }
+
+        let mut gen_writer =
+            Writer::new(unsafe { writer.as_mut_unchecked() });
+
+        gen_writer.write_str("Hello, World!").unwrap();
+        gen_writer.write_str("Hello, World!").unwrap();
+
+        std::println!("Screen Start: \n{:?}", unsafe {
+            writer.as_ref_unchecked().screen_start
+        });
+
+        std::println!("Cursor: \n{:?}", unsafe {
+            writer.as_ref_unchecked().cursor
+        });
+
+        gen_writer.inner.update();
+        let b = unsafe { backing.as_ref_unchecked() };
+
+        for char in 0..2000 {
+            if char % 80 == 0 && char != 0 {
+                std::println!("|");
+            }
+            if b[char].char == Char::LineFeed {
+                continue;
+            }
+
+            std::print!("{}", unsafe { b[char].char });
+        }
+        std::println!();
+    }
 }
