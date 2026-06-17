5aefd7487740b21d4adeef13e8c372cd486247ea
sagi <sagi21805@gmail.com>
2026-06-17 12:26:23 +0300

    Add releax strategy

--- crates/sync/src/mutex.rs
+++ crates/sync/src/mutex.rs
@@ -1,38 +1,49 @@
-use core::{cell::UnsafeCell, ops::{Deref, DerefMut}, sync::atomic::{AtomicBool, Ordering}};
+use core::{
+    cell::UnsafeCell,
+    marker::PhantomData,
+    ops::{Deref, DerefMut},
+    sync::atomic::{AtomicBool, Ordering},
+};
 
-pub struct SpinLockMutex<T> {
+use crate::strategy::{RelaxStrategy, Spin};
+
+pub struct SpinLockMutex<T, R: RelaxStrategy = Spin> {
+    strategy: PhantomData<R>,
     locked: AtomicBool,
     data: UnsafeCell<T>,
 }
 
-unsafe impl<T> Sync for SpinLockMutex<T> where T: Send {}
+unsafe impl<T, S: RelaxStrategy> Sync for SpinLockMutex<T, S> where T: Send {}
 
 impl<T> SpinLockMutex<T> {
     pub const fn new(data: T) -> Self {
         Self {
+            strategy: PhantomData,
             locked: AtomicBool::new(false),
             data: UnsafeCell::new(data),
         }
     }
 
-    pub fn lock(&self) -> Guard<'_, T> {
-        // While the lock is `true`, a swap to `true` returns the previous value which is `true`
-        // which keeps the mutex locked.
-        // Only when unlocking, the swap would return `false` which would stop the loop
+    pub fn lock(&self) -> SpinlockGuard<'_, T> {
+        // While the lock is `true`, a swap to `true` returns the previous
+        // value which is `true` which keeps the mutex locked.
+        // Only when unlocking, the swap would return `false` which would
+        // stop the loop
+        let mut tick: usize = 0;
         while self.locked.swap(true, Ordering::Acquire) {
-            core::hint::spin_loop();
+            tick += 1;
+            Spin::relax(tick);
         }
 
-        Guard { mutex: self }
+        SpinlockGuard { mutex: self }
     }
-
 }
 
-pub struct Guard<'a, T> {
+pub struct SpinlockGuard<'a, T> {
     mutex: &'a SpinLockMutex<T>,
 }
 
-impl<T> Deref for Guard<'_, T> {
+impl<T> Deref for SpinlockGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -40,13 +51,13 @@ impl<T> Deref for Guard<'_, T> {
     }
 }
 
-impl<T> DerefMut for Guard<'_, T> {
+impl<T> DerefMut for SpinlockGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe { &mut *self.mutex.data.get() }
     }
 }
 
-impl<T> Drop for Guard<'_, T> {
+impl<T> Drop for SpinlockGuard<'_, T> {
     fn drop(&mut self) {
         self.mutex.locked.store(false, Ordering::Release);
     }
