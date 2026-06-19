dd5eb10b83f26bc4ad1fc852dfaaad8bbcf695f8
sagi <sagi21805@gmail.com>
2026-06-19 08:30:50 +0300

    Put branch to a stable state for the fix of the new screen

--- crates/drivers/vga-display/src/color_code.rs
+++ crates/drivers/vga-display/src/color_code.rs
@@ -1,19 +1,12 @@
 use common::enums::Color;
 use macros::bitfields;
 
-#[derive(Clone, Copy)]
-pub struct ColorCode(u8);
-
-impl ColorCode {
-    /// Set the VGA char Background and Foreground
-    ///
-    /// # Parameters
-    ///
-    /// - `foreground`: The color of the character itself
-    /// - `background`: The background color of the character
-    pub const fn new(foreground: Color, background: Color) -> Self {
-        Self((background as u8) << 4 | (foreground as u8))
-    }
+#[bitfields]
+pub struct ColorCode {
+    #[flag(flag_type = Color)]
+    pub foreground: B4,
+    #[flag(flag_type = Color)]
+    pub background: B4,
 }
 
 #[rustfmt::skip]
