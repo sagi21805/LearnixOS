dd7579211bfe0c932da05fe05b1422f75bd65830
sagi <sagi21805@gmail.com>
2026-07-13 21:24:41 +0300

    Changed bump allocator to be truely sync on the allocate function

--- crates/memory/bump/src/lib.rs
+++ crates/memory/bump/src/lib.rs
@@ -1,27 +1,26 @@
 #![no_std]
 #![feature(ptr_alignment_type)]
 
-use core::{
-    alloc::{GlobalAlloc, Layout},
-    sync::atomic::{AtomicUsize, Ordering},
-};
+use core::alloc::{GlobalAlloc, Layout};
 
 use common::{
     address_types::{Address, PhysicalAddress},
+    constants::REGULAR_PAGE_ALIGNMENT,
     enums::MemoryRegionType,
 };
 
+use sync::mutex::SpinMutex;
 use x86::memory_map::MemoryMap;
 
 pub struct BumpAllocator<'a> {
-    curser: AtomicUsize,
+    curser: SpinMutex<usize>,
     mmap: &'a MemoryMap,
 }
 
 impl<'a> BumpAllocator<'a> {
     pub fn new(mmap: &'a MemoryMap) -> BumpAllocator<'a> {
         BumpAllocator {
-            curser: AtomicUsize::new(0),
+            curser: SpinMutex::new(0),
             mmap,
         }
     }
@@ -29,11 +28,13 @@ impl<'a> BumpAllocator<'a> {
 
 unsafe impl<'a> GlobalAlloc for BumpAllocator<'a> {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut curser = self.curser.lock();
+
+        // Allocate at least a page per allocation.
+        let alignment = layout.alignment().max(REGULAR_PAGE_ALIGNMENT);
+
         let mut aligned_cursor = unsafe {
-            PhysicalAddress::new_unchecked(
-                self.curser.load(Ordering::Relaxed),
-            )
-            .align_up(layout.alignment())
+            PhysicalAddress::new_unchecked(*curser).align_up(alignment)
         };
 
         let regions = self.mmap.regions.read();
@@ -48,7 +49,7 @@ unsafe impl<'a> GlobalAlloc for BumpAllocator<'a> {
                         PhysicalAddress::new_unchecked(
                             b.base_address as usize,
                         )
-                        .align_up(layout.alignment())
+                        .align_up(alignment)
                     }
                 }
 
@@ -60,10 +61,7 @@ unsafe impl<'a> GlobalAlloc for BumpAllocator<'a> {
             });
 
         if memmap_block.is_some() {
-            self.curser.store(
-                aligned_cursor.as_usize() + layout.size(),
-                Ordering::Relaxed,
-            );
+            *curser = aligned_cursor.as_usize() + layout.size();
             unsafe { aligned_cursor.as_non_null().as_mut() }
         } else {
             core::ptr::null_mut()
