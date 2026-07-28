7596f6bf14853c9a78952fac599cf8dc25c88076
sagi <sagi21805@gmail.com>
2026-07-28 14:59:40 +0300

    moved preallocated to it's own file.

--- crates/memory/slab/src/descriptor.rs
+++ crates/memory/slab/src/descriptor.rs
@@ -11,21 +11,6 @@ use core::{
 use macros::bitfields;
 use nonmax::{NonMaxU16, NonMaxU32};
 
-/// Preallocated object in the slab allocator.
-pub union PreAllocated<T: Sized> {
-    pub allocated: ManuallyDrop<T>,
-    pub next_free_idx: Option<NonMaxU16>,
-}
-
-impl<T: Debug> Debug for PreAllocated<T> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_struct("PreAllocated")
-            .field("allocated", unsafe { &self.allocated })
-            .field("next_free_idx", unsafe { &self.next_free_idx })
-            .finish()
-    }
-}
-
 #[derive(Clone, Copy)]
 struct Free {
     next_free_idx: Option<NonMaxU32>,
--- crates/memory/slab/src/lib.rs
+++ crates/memory/slab/src/lib.rs
@@ -10,6 +10,7 @@
 pub mod cache;
 pub mod descriptor;
 pub mod local_macros;
+pub mod preallocated;
 pub mod traits;
 
 use crate::{
