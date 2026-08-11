78dae0f52703327949d555d03566dc583f03b3b1
sagi <sagi21805@gmail.com>
2026-08-11 21:07:32 +0300

    moved specific desciptor state implemenation to seprate files and added detached version of each state

--- crates/memory/slab/src/descriptor.rs
+++ crates/memory/slab/src/descriptor.rs
@@ -1,14 +1,16 @@
 extern crate alloc;
 
-use crate::{preallocated::PreAllocated, traits::Slab};
-use alloc::alloc::{Layout, alloc};
-use common::{
-    address_types::{Address, VirtualAddress},
-    constants::{REGULAR_PAGE_ALIGNMENT, REGULAR_PAGE_SIZE},
+pub mod free;
+pub mod full;
+pub mod partial;
+
+use crate::{
+    preallocated::PreAllocated,
+    traits::{DetachedSlabState, Slab, SlabState},
 };
-use core::{mem::size_of, num::NonZeroU64, ptr::NonNull};
+use common::address_types::{Address, VirtualAddress};
+use core::{num::NonZeroU64, ptr::NonNull};
 use macros::bitfields;
-use nonmax::NonMaxU16;
 
 #[repr(C)]
 pub struct SlabDescriptor<T, S>
@@ -24,32 +26,69 @@ where
     // Moreover the local T holds the const order in which we allocate for
     // the array so the size can always be calculated.
     pub objects: NonNull<PreAllocated<T>>,
-    pub next: Option<NonNull<SlabDescriptor<T, S>>>,
-}
-
-// TODO: Seal trait
-pub trait SlabState<T: Slab> {
-    type Meta: Sized;
+    pub next: Option<NonNull<S::Next>>,
 }
 
 /// Partial slab is a slab that has some allocated objects, and some free
 /// objects.
 pub struct Partial;
+pub struct PartialDetached;
+
+impl<T: Slab> DetachedSlabState<T> for PartialDetached {
+    type Attached = Partial;
+}
+
 impl<T: Slab> SlabState<T> for Partial {
     type Meta = PartialMeta;
+    type DetachedState = PartialDetached;
+}
+
+impl<T: Slab> SlabState<T> for PartialDetached {
+    type Meta = RawMeta;
+    type Next = ();
+    type Detached = ();
+    type DetachedState = ();
 }
 
 /// Free slab is a slab that does not allocate any objects, and is
 /// initialized that the first allocatable index is 0.
 pub struct Free;
+pub struct FreeDetached;
+
+impl<T: Slab> DetachedSlabState<T> for FreeDetached {
+    type Attached = Free;
+}
+
 impl<T: Slab> SlabState<T> for Free {
     type Meta = FullFreeMeta;
+    type DetachedState = FreeDetached;
+}
+
+impl<T: Slab> SlabState<T> for FreeDetached {
+    type Meta = RawMeta;
+    type Next = ();
+    type Detached = ();
+    type DetachedState = ();
 }
 
 /// Full slab is a slab that is fully allocated.
 pub struct Full;
+pub struct FullDetached;
+
+impl<T: Slab> DetachedSlabState<T> for FullDetached {
+    type Attached = Full;
+}
+
 impl<T: Slab> SlabState<T> for Full {
     type Meta = FullFreeMeta;
+    type DetachedState = FullDetached;
+}
+
+impl<T: Slab> SlabState<T> for FullDetached {
+    type Meta = RawMeta;
+    type Next = ();
+    type Detached = ();
+    type DetachedState = ();
 }
 
 /// A used slab may be full or partial.
@@ -59,22 +98,9 @@ impl<T: Slab> SlabState<T> for Full {
 pub struct Used;
 impl<T: Slab> SlabState<T> for Used {
     type Meta = RawMeta;
-}
-
-pub trait Attach<T: Slab, D: SlabState<T>> {
-    fn attach(&mut self, other: &mut SlabDescriptor<T, D>);
-}
-
-pub trait Detach<T: Slab, D: SlabState<T>> {
-    fn detach(&mut self);
-}
-
-/// Convert from
-pub trait ConvertInplace<T: Slab, D: SlabState<T>> {
-    fn convert_inplace(
-        &mut self,
-        meta: D::Meta,
-    ) -> &mut SlabDescriptor<T, D>;
+    type Next = ();
+    type Detached = ();
+    type DetachedState = ();
 }
 
 pub enum SlabStateKind {
@@ -141,141 +167,17 @@ pub struct FullFreeMeta {
     pub partial: B1,
 }
 
-impl<T: Slab> SlabDescriptor<T, Free> {
-    /// Create a new, free slab descriptor.
-    pub fn new(
-        order: usize,
-        next: Option<NonNull<SlabDescriptor<T, Free>>>,
-    ) -> SlabDescriptor<T, Free> {
-        let address = unsafe {
-            NonNull::new_unchecked(alloc(
-                Layout::from_size_alignment_unchecked(
-                    REGULAR_PAGE_SIZE * (1 << order),
-                    REGULAR_PAGE_ALIGNMENT,
-                ),
-            ))
-            .cast::<PreAllocated<T>>()
-        };
-
-        let mut objects = NonNull::slice_from_raw_parts(
-            address,
-            (REGULAR_PAGE_SIZE * (1 << order))
-                / size_of::<PreAllocated<T>>(),
-        );
-
-        // Initialize each free object to point at the next free.
-        for (i, object) in
-            unsafe { objects.as_mut() }.iter_mut().enumerate()
-        {
-            *object = PreAllocated {
-                next_free_idx: Some(unsafe {
-                    NonMaxU16::new_unchecked(i as u16 + 1)
-                }),
-            }
-        }
-
-        unsafe {
-            // Set the last object free_index into none because it is the
-            // end of the slab.
-            objects.as_mut().last_mut().unwrap().next_free_idx = None;
-
-            SlabDescriptor {
-                state: FullFreeMeta::new()
-                    .prev(SlabAddress(None))
-                    .partial(false),
-                objects: objects.cast(),
-                next,
-            }
-        }
-    }
-}
-
-impl<T: Slab> SlabDescriptor<T, Partial> {
-    /// Allocate an object from this slab returning the allocated object
-    /// and state of the slab.
-    ///
-    /// # Parameters
-    ///
-    /// * `head` - The head of the full slab descriptor. This slab will be
-    ///   attached to it, if the allocation will make this slab full.
-    pub fn alloc(&mut self) -> (NonNull<T>, SlabStateKind) {
-        debug_assert!(
-            self.state.is_partial(),
-            "Compiletime state does not match runtime state"
-        );
-
-        let idx = self.state.get_next_free_idx() as usize;
-        let preallocated =
-            unsafe { self.objects.add(idx as usize).as_mut() };
-
-        let mut final_state = SlabStateKind::Partial;
-
-        self.state
-            .set_total_allocated(self.state.get_total_allocated() + 1);
-
-        match unsafe { preallocated.next_free_idx } {
-            Some(idx) => {
-                self.state.set_next_free_idx(idx.get());
-            }
-            None => {
-                final_state = SlabStateKind::Full;
-            }
-        }
-
-        unsafe {
-            (NonNull::from_mut(&mut preallocated.allocated), final_state)
-        }
-    }
-
-    /// Deallocate an object  from this slab.
-    ///
-    /// # Parameters
-    ///
-    /// * `idx` - The index of the object to deallocate.
-    pub unsafe fn dealloc(&mut self, idx: NonMaxU16) -> SlabStateKind {
-        unsafe {
-            self.objects.add(idx.get() as usize).as_mut().next_free_idx =
-                NonMaxU16::new(self.state.get_next_free_idx());
-        }
-
-        self.state.set_next_free_idx(idx.get());
-
-        self.state
-            .set_total_allocated(self.state.get_total_allocated() - 1);
-
-        if self.state.get_total_allocated() == 0 {
-            SlabStateKind::Free
-        } else {
-            SlabStateKind::Partial
-        }
-    }
-}
-
-impl<T> SlabDescriptor<T, Free>
-where
-    T: Slab,
-{
-    pub fn alloc(&mut self) -> (NonNull<T>, SlabStateKind) {
-        self.detach();
-
-        let partial: &mut SlabDescriptor<T, Partial> =
-            unsafe { core::mem::transmute(self) };
-
-        partial.state = PartialMeta::new()
-            .partial(true)
-            .next_free_idx(0)
-            .total_allocated(0);
-
-        partial.alloc()
-    }
-}
-
+/// Shared linked-list attach/detach logic for the two state kinds
+/// (`Free` and `Full`) that use `FullFreeMeta` as their metadata.
 impl<T, S> SlabDescriptor<T, S>
 where
     T: Slab,
-    S: SlabState<T, Meta = FullFreeMeta>,
+    S: SlabState<T, Meta = FullFreeMeta, Next = Self>,
 {
-    fn attach_linked(&mut self, other: &mut SlabDescriptor<T, S>) {
+    pub(crate) fn attach_linked(
+        &mut self,
+        other: &mut SlabDescriptor<T, S>,
+    ) {
         other.next = self.next;
         other
             .state
@@ -290,58 +192,20 @@ where
         self.next = Some(NonNull::from_mut(other));
     }
 
-    fn detach_linked(&mut self) {
+    pub(crate) fn detach_linked(&mut self) -> &mut S::Detached {
         if let Some(mut next) = self.next {
             unsafe { next.as_mut().state = self.state };
         }
 
         if let Some(mut prev) =
-            unsafe { self.state.get_prev().as_non_null() }
+            unsafe { self.state.get_prev().as_non_null::<T, S>() }
         {
             unsafe { prev.as_mut() }.next = self.next;
         }
 
         self.next = None;
-    }
-}
 
-impl<T: Slab> Attach<T, Full> for SlabDescriptor<T, Full> {
-    fn attach(&mut self, other: &mut SlabDescriptor<T, Full>) {
-        self.attach_linked(other);
-    }
-}
-
-impl<T: Slab> Detach<T, Full> for SlabDescriptor<T, Full> {
-    fn detach(&mut self) { self.detach_linked(); }
-}
-
-impl<T: Slab> Detach<T, Free> for SlabDescriptor<T, Free> {
-    fn detach(&mut self) { self.detach_linked(); }
-}
-
-impl<T: Slab> Attach<T, Free> for SlabDescriptor<T, Free> {
-    fn attach(&mut self, other: &mut SlabDescriptor<T, Free>) {
-        self.attach_linked(other);
-    }
-}
-
-impl<T: Slab> Attach<T, Partial> for SlabDescriptor<T, Free> {
-    fn attach(&mut self, other: &mut SlabDescriptor<T, Partial>) {
-        let free = other.convert_inplace(
-            FullFreeMeta::new()
-                .partial(false)
-                .prev(SlabAddress::from_non_null(NonNull::from_mut(self))),
-        );
-
-        self.attach_linked(free);
-    }
-}
-
-impl<T: Slab> Attach<T, Partial> for SlabDescriptor<T, Partial> {
-    fn attach(&mut self, other: &mut SlabDescriptor<T, Partial>) {
-        other.next = self.next;
-
-        self.next = Some(NonNull::from_mut(other));
+        unsafe { core::mem::transmute(self) }
     }
 }
 
@@ -368,37 +232,5 @@ impl<T: Slab> SlabDescriptor<T, Used> {
         } else {
             Err(unsafe { core::mem::transmute(self) })
         }
-    }
-}
-
-impl<T: Slab> ConvertInplace<T, Free> for SlabDescriptor<T, Partial> {
-    fn convert_inplace(
-        &mut self,
-        meta: FullFreeMeta,
-    ) -> &mut SlabDescriptor<T, Free> {
-        let free: &mut SlabDescriptor<T, Free> =
-            unsafe { core::mem::transmute(self) };
-
-        assert!(!meta.is_partial());
-
-        free.state = meta;
-
-        free
-    }
-}
-
-impl<T: Slab> ConvertInplace<T, Partial> for SlabDescriptor<T, Free> {
-    fn convert_inplace(
-        &mut self,
-        meta: PartialMeta,
-    ) -> &mut SlabDescriptor<T, Partial> {
-        let partial: &mut SlabDescriptor<T, Partial> =
-            unsafe { core::mem::transmute(self) };
-
-        assert!(meta.is_partial());
-
-        partial.state = meta;
-
-        partial
     }
 }
